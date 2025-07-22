42f6bf69c8eb01de3c72c645fb7687cd2faf4f4d
Amiir25 <amirsadik636@gmail.com>
2025-07-22 22:26:44 +0300

    Add Home.jsx

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Home = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Home
+
+/**
+ * - Favorite jobs
+ * - "Keep Your Job Search Organized"
+ * - <a href="https://storyset.com/job">Job illustrations by Storyset</a>
+ * 
+ * - Links: AI Resume Builder | AI Resume Review | Job Application Tracker
+ */
