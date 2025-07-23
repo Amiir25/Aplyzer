0ccd46624aa68b02919c7b46f8a305ad8d6a84fb
Amiir25 <amirsadik636@gmail.com>
2025-07-23 09:44:50 +0300

    Update Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,14 +3,26 @@ import React from 'react'
 const Hero = () => {
   return (
     <>
-    <div className='border h-screen flex flex-col items-center pt-50'>
+    <div className='max-h-screen flex flex-col items-center pt-40 md:pt-30 text-center px-6'>
         
-        <h1>Track Jobs. Optimize Resumes. Get Hired.</h1>
-        <p>From first application to final offer — Aplyzer keeps you organized and ahead with smart career tools.</p>
+        <h1 className='text-5xl md:text-7xl font-bold'>Track Jobs. Optimize Resumes. Get Hired.</h1>
+        <p className='text-xl md:text-3xl font-light my-4'>
+          From first application to final offer —
+          <span className='underline decoration-[#F51D28] mx-2'>Aplyzer</span>
+          keeps you organized and ahead with smart career tools.
+        </p>
 
-        <div>
-          <button>Get Started Free</button>
-          <button>See How It Works</button>
+        {/* Hero buttons */}
+        <div className='mt-10 flex gap-12'>
+          
+          <button className='text-lg md:text-2xl font-medium px-4 py-2 md:px-6 md:py-4 rounded bg-[#F51D28] text-white hover:opacity-90 '>
+            Get Started Free
+          </button>
+
+          <button className='text-lg md:text-2xl font-medium border px-4 py-2 md:px-6 md:py-4 rounded border-[#F51D28] hover:text-gray-600 '>
+            See How It Works
+          </button>
+
         </div>
 
     </div>
