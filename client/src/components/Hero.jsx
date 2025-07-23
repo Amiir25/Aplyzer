48b7d036660e96fd4e385b9b32ab7290d6df364b
Amiir25 <amirsadik636@gmail.com>
2025-07-23 07:59:05 +0300

    Add Hero.jsx

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+    <div className='border h-screen flex flex-col items-center pt-50'>
+        
+        <h1>Track Jobs. Optimize Resumes. Get Hired.</h1>
+        <p>From first application to final offer — Aplyzer keeps you organized and ahead with smart career tools.</p>
+
+        <div>
+          <button>Get Started Free</button>
+          <button>See How It Works</button>
+        </div>
+
+    </div>
+    </>
+  )
+}
+
+export default Hero
