cac8203c9955a14576a703713e243e695acc8d1d
Amiir25 <amirsadik636@gmail.com>
2025-07-23 07:58:47 +0300

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,24 +1,29 @@
-import React from "react";
-import { assets } from "../assets/assets.js";
+import React, { useState } from "react";
+// import { assets } from "../assets/assets.js";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown, faChartBar, faCheckDouble, faEquals, faPeace, faPen } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDown, faBars, faChartBar, faCheckDouble, faCircleXmark, faEquals, faPen } from "@fortawesome/free-solid-svg-icons";
 
 
 
 const Navbar = () => {
+
+    const [smallScreen, setSmallScreen] = useState(false);
+
     return (
         <>
-        <nav className="py-2 flex items-center justify-between text-gray-800 md:text-lg font-medium">
+        <nav className="relative py-4 flex items-center justify-between text-gray-800 md:text-lg font-medium">
 
             {/* Logo */}
-            <img src="/aplyzer-transparent.png" alt="Logo Image" className="w-40" />
+            <Link to={'/'} className="w-1/4 md:w-1/5">
+                <img src="/logo-header.png" alt="Logo Image" />
+            </Link>
 
             {/* Links */}
-            <div className="relative gap-8 hidden md:flex pt-4">
-                <Link>About</Link>
-                <Link>Tips</Link>
-                <Link>Contact</Link>
+            <div className="relative gap-8 hidden md:flex">
+                <Link to='/' className="hover:text-gray-600">About</Link>
+                <Link to='/' className="hover:text-gray-600">Tips</Link>
+                <Link to='/' className="hover:text-gray-600">Contact</Link>
 
                 {/* More tools */}
                 <div className="group">
@@ -27,9 +32,12 @@ const Navbar = () => {
                         <FontAwesomeIcon icon={ faAngleDown } />
                     </button>
 
-                    <div className="absolute -top-100 left-0 right-0 border-b border-gray-400 px-2 pt-6 opacity-0 group-hover:top-full group-hover:opacity-100 transform translate-y-10 transition-all duration-500 z-10">
+                    <div className="absolute top-full left-0 right-0 px-2 pt-6 opacity-0 max-h-0 z-10
+                    transform translate-y-full overflow-hidden transition-all ease-in-out duration-200
+                    group-hover:max-h-82 group-hover:translate-y-0 group-hover:border-b border-gray-400
+                    group-hover:opacity-100">
                         
-                        <Link to='/' className="flex items-center gap-4 mb-6">
+                        <Link to='/' className="flex items-center gap-4 mb-6 hover:text-gray-600">
                             <FontAwesomeIcon icon={ faChartBar } className="w-8 p-2 bg-rose-400/50 text-rose-700
                             rounded text-2xl" />
                             <div>
@@ -38,7 +46,7 @@ const Navbar = () => {
                             </div>
                         </Link>
 
-                        <Link to='/' className="flex items-center gap-4 mb-6">
+                        <Link to='/' className="flex items-center gap-4 mb-6 hover:text-gray-600">
                             <FontAwesomeIcon icon={ faCheckDouble } className="w-8 p-2 bg-yellow-400/50 text-yellow-700
                             rounded text-2xl" />
                             <div>
@@ -47,7 +55,7 @@ const Navbar = () => {
                             </div>
                         </Link>
 
-                        <Link to='/' className="flex items-center gap-4 mb-6">
+                        <Link to='/' className="flex items-center gap-4 mb-6 hover:text-gray-600">
                             <FontAwesomeIcon icon={ faEquals } className="w-8 p-2 bg-lime-400/50 text-lime-700
                             rounded text-2xl" />
                             <div>
@@ -56,7 +64,78 @@ const Navbar = () => {
                             </div>
                         </Link>
 
-                        <Link to='/' className="flex items-center gap-4 mb-6">
+                        <Link to='/' className="flex items-center gap-4 mb-6 hover:text-gray-600">
+                            <FontAwesomeIcon icon={ faPen } className="w-8 p-2 bg-sky-400/50 text-sky-700
+                            rounded text-2xl" />
+                            <div>
+                                <h2 className="text-sm font-bold">Cover Letter Generator</h2>
+                                <p className="text-xs">Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                        </Link>
+                    </div>
+                </div> {/* more tools */}
+            </div> {/* links */}
+
+            {/* Sign */}
+            <button className="hidden md:block border border-[#F51D28] px-4 py-2 rounded
+            hover:text-white hover:bg-[#F51D28] ">
+                <Link>Sign up</Link>
+            </button>
+
+            {/* Small screen menu */}
+            <div>
+                <FontAwesomeIcon icon={ faBars } className="md:hidden text-2xl hover:text-gray-600 cursor-pointer"
+                onClick={() => setSmallScreen(true)} />
+            </div>
+
+            {/* Small screen links */}
+            <div className={`bg-gray-700 absolute top-0 -left-10 w-screen h-screen transform -translate-x-200
+            ${ smallScreen && 'translate-x-0' } transition-all duration-300`}>
+                
+                {/* Close icon */}
+                <FontAwesomeIcon icon={ faCircleXmark } className="float-right text-3xl p-6"
+                onClick={ () => setSmallScreen(false) } />
+
+                <Link to='/' className="hover:text-gray-600">About</Link>
+                <Link to='/' className="hover:text-gray-600">Tips</Link>
+                <Link to='/' className="hover:text-gray-600">Contact</Link>
+
+                {/* More tools */}
+                <div className="group">
+                    <button className=" flex items-center gap-2 hover:text-[#F51D28]">
+                        More
+                        <FontAwesomeIcon icon={ faAngleDown } />
+                    </button>
+
+                    <div>
+                        <Link to='/' className="flex items-center gap-4 mb-6 hover:text-gray-600">
+                            <FontAwesomeIcon icon={ faChartBar } className="w-8 p-2 bg-rose-400/50 text-rose-700
+                            rounded text-2xl" />
+                            <div>
+                                <h2 className="text-sm font-bold">Job Application Tracker</h2>
+                                <p className="text-xs">Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                        </Link>
+
+                        <Link to='/' className="flex items-center gap-4 mb-6 hover:text-gray-600">
+                            <FontAwesomeIcon icon={ faCheckDouble } className="w-8 p-2 bg-yellow-400/50 text-yellow-700
+                            rounded text-2xl" />
+                            <div>
+                                <h2 className="text-sm font-bold">AI Resume Cheker</h2>
+                                <p className="text-xs">Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                        </Link>
+
+                        <Link to='/' className="flex items-center gap-4 mb-6 hover:text-gray-600">
+                            <FontAwesomeIcon icon={ faEquals } className="w-8 p-2 bg-lime-400/50 text-lime-700
+                            rounded text-2xl" />
+                            <div>
+                                <h2 className="text-sm font-bold">Job Description Matching</h2>
+                                <p className="text-xs">Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                        </Link>
+
+                        <Link to='/' className="flex items-center gap-4 mb-6 hover:text-gray-600">
                             <FontAwesomeIcon icon={ faPen } className="w-8 p-2 bg-sky-400/50 text-sky-700
                             rounded text-2xl" />
                             <div>
@@ -68,11 +147,6 @@ const Navbar = () => {
                 </div>
             </div>
 
-            {/* Sign */}
-            <button className="hidden md:block border border-[#F51D28] px-4 py-2 rounded
-            hover:text-white hover:bg-[#F51D28] ">
-                <Link>Sign up</Link>
-            </button>
         </nav>
         </>
     )
