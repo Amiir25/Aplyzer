2a0a31958c8e8cc439b13e0ab0124f478b64ba48
Amiir25 <amirsadik636@gmail.com>
2025-07-24 15:45:01 +0300

    Update LookInside.jsx

--- client/src/components/LookInside.jsx
+++ client/src/components/LookInside.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import { assets, lookInside } from '../assets/assets'
+import Title from './Title'
 
 const LookInside = () => {
   return (
     <>
-    <div style={{ backgroundImage: `url(${lookInside.image4})` }}
-    className={`h-screen bg-cover bg-center bg-no-repeat bg-fixed`}>
+    <div className='text-center'>
+      <Title 
+      title="Look Inside"
+      subtitle="Explore how Aplyzer helps you stay on top of every opportunity with a clean, intuitive interface." />
+
+      <div style={{ backgroundImage: `url(${lookInside.image4})` }}
+      className={`h-screen bg-cover bg-center bg-no-repeat bg-fixed`}>
+      </div>
+
+      <button className='border w-1/3 py-2 mt-10 md:text-2xl font-medium border border-[#F51D28]
+      hover:text-gray-200 hover:bg-[#F51D28] '>
+        Try it now!
+      </button>
     </div>
     </>
   )
