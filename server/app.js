c9964048987613ea457958f4ef408f8399928ae1
Amiir25 <amirsadik636@gmail.com>
2025-07-25 16:50:18 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-// import { db } from './databse/db.js';
+
+import authRouter from './routes/authRouter.js';
 
 dotenv.config();
-import jobsRouter from './routes/jobs.routes.js';
-
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/jobs', jobsRouter);
+// Routes
+app.use('/auth', authRouter);
 
 const port = process.env.PORT;
 app.listen(port, () => {
