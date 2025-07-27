b054e8792ab2b2f9a9075cced7adefffb394dca0
Amiir25 <amirsadik636@gmail.com>
2025-07-27 15:27:40 +0300

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,10 +1,39 @@
-import mysql from 'mysql2';
 import db from "../database/db.js";
+import bcrypt from 'bcryptjs';
 
 export const signUp = (req, res, next) => {
-    const { name, email, password } = req.body;
-    console.log(email);
-    res.status(200).json({message: 'Sign Up successful'});
+    const { username, email, password } = req.body;
+    try {
+
+        // Check if the user exists
+        const checkQuery = 'SELECT * FROM users WHERE username = ? OR email = ?';
+        db.query(checkQuery, [username, email], async (err, user) => {
+            if (err) {
+                console.error('DB error', err);
+                return res.status(500).json({ message: 'Database error' });
+            }
+
+            if (user.length) return res.status(409).json({ message: 'Username or email already exists' });
+
+            // Hash password
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            // Insert new user
+            const insertQuery = 'INSERT INTO users (username, email, password) VALUES (?)';
+            const values = [username, email, hashedPassword];
+            db.query(insertQuery, [values], (err) => {
+                if (err) {
+                    console.error("Insert error:", err);
+                    return res.status(500).json({ message: 'Failed to create user' })
+                }
+                res.status(201).json({ message: 'User has been created succussfully' });
+            });
+        });
+        
+    } catch (err) {
+        console.error('Server error', err);
+        res.status(500).json({Error: 'Internal server error'});
+    }
 }
 
 export const signIn = (req, res, next) => {
