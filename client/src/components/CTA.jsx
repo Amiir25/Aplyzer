76f89a6f9ab099cef972775060610dc19506ea8a
Amiir25 <amirsadik636@gmail.com>
2025-07-29 11:16:20 +0300

    Update the main color on CTA.jsx

--- client/src/components/CTA.jsx
+++ client/src/components/CTA.jsx
@@ -13,11 +13,11 @@ const CTA = () => {
         {/* CTA buttons */}
         <div className='flex justify-center gap-12'>
           
-          <button className='text-lg md:text-2xl font-medium px-4 py-2 md:px-6 md:py-4 rounded bg-[#F51D28] text-white hover:opacity-90 '>
+          <button className='text-lg md:text-2xl font-medium px-4 py-2 md:px-6 md:py-4 rounded bg-gradient-main text-white hover:opacity-90 '>
             Get Started Free
           </button>
 
-          <button className='text-lg md:text-2xl font-medium border px-4 py-2 md:px-6 md:py-4 rounded border-[#F51D28] hover:text-gray-600 '>
+          <button className='text-lg md:text-2xl font-medium border px-4 py-2 md:px-6 md:py-4 rounded border-[#02A9EB] hover:text-gray-600 '>
             See How It Works
           </button>
         </div>
