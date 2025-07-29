344274d3c8629b128c112a1e7c14ff4f5524537b
Amiir25 <amirsadik636@gmail.com>
2025-07-29 11:15:36 +0300

    Update the main color on LookInside.jsx

--- client/src/components/LookInside.jsx
+++ client/src/components/LookInside.jsx
@@ -14,8 +14,8 @@ const LookInside = () => {
       className={`h-screen bg-cover bg-center bg-no-repeat bg-fixed`}>
       </div>
 
-      <button className='border w-1/3 py-2 mt-10 md:text-2xl font-medium border border-[#F51D28]
-      hover:text-gray-200 hover:bg-[#F51D28] '>
+      <button className='border w-1/3 py-2 mt-10 md:text-2xl font-medium rounded border border-[#02A9EB]
+      hover:text-gray-200 hover:bg-gradient-main '>
         Try it now!
       </button>
     </div>
