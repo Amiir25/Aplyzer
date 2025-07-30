e4d8a9c8ffe34ff98e0525e141a435c2e5e69c6c
Amiir25 <amirsadik636@gmail.com>
2025-07-30 09:12:55 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
 import Home from './pages/Home'
-import { Routes, Route } from 'react-router-dom'
+import { useLocation, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Footer from './components/Footer'
-import Dashboard from './pages/Dashboard'
+import Dashboard from './user/Dashboard'
 
 const App = () => {
+
+  const location = useLocation();
+
   return (
     <div className=''>
 
       {/* Navbar */}
-      <Navbar/>
+      { !location.pathname.includes('/user') && <Navbar/> }
       
       <Routes>
+
+        {/* Before Login */}
         <Route path='/' element={ <Home/> } />
         <Route path='/auth/signup' element={ <SignUp/> } />
         <Route path='/auth/signin' element={ <SignIn/> } />
+
+        {/* After Login */}
         <Route path='/user/dashboard/:userId' element={ <Dashboard/> } />
       </Routes>
       
