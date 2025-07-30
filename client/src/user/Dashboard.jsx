c1f545e2ca30d3dba539bc2273428be16969f4a4
Amiir25 <amirsadik636@gmail.com>
2025-07-30 09:13:45 +0300

    Add Dashboard.jsx

---
+++ client/src/user/Dashboard.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios';
+import UserNavbar from './UserNavbar';
+
+const Dashboard = () => {
+    const { userId } = useParams();
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3000/user/dashboard/${userId}`);
+                setUser(response.data.user);
+                console.log(response.data.user.email);
+            } catch (error) {
+                const errorMsg = error.response?.data?.message;
+            }
+        }
+        fetchUserData();
+    }, [userId]);
+
+    return (
+        <>
+        {/* Navbar */}
+        <UserNavbar user={ user }/>
+        
+        <div className='relative px-6 md:px-12 lg:px-24 xl:px-32 mt-10'>
+            <h1>Hi { user.username }</h1>
+            <p>You have 8 active job applications. Keep going!</p>
+
+            {/* Quick Stats */}
+            <div className='absolute top-0 right-32 w-fit px-4 py-2 shadow-xl border-t-6 border-[#02A9EB]'>
+                <h1 className='text-2xl font-semibold mb-6'>Quick Stats</h1>
+                <div className='grid grid-cols-2 gap-8 mb-4'>
+                    <p>Application Sent</p>
+                    <p>12</p>
+                </div>
+                <div className='grid grid-cols-2 gap-8 mb-4'>
+                    <p>Interviews Scheduled</p>
+                    <p>3</p>
+                </div>
+                <div className='grid grid-cols-2 gap-8 mb-4'>
+                    <p>Rejected</p>
+                    <p>4</p>
+                </div>
+                <div className='grid grid-cols-2 gap-8 mb-4'>
+                    <p>Offers</p>
+                    <p>1</p>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Dashboard
