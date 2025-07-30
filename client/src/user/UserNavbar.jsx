b928b2e99916f350c673750a63c0c3f5ea36dbf4
Amiir25 <amirsadik636@gmail.com>
2025-07-30 09:14:06 +0300

    Add UserNavbar.jsx

---
+++ client/src/user/UserNavbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom';
+
+const UserNavbar = ({ user }) => {
+
+  const location = useLocation();
+  return (
+    <>
+    <nav className="relative px-6 md:px-16 lg:px-24 xl:px-32 py-4 flex items-center justify-between
+      text-gray-800 md:text-lg font-medium">
+
+        {/* Logo */}
+        <Link to={'/'} className="w-1/6 md:w-1/7">
+            <img src="/logo-2.PNG" alt="Logo Image" />
+        </Link>
+
+        {/* Links */}
+        <div className="relative gap-8 hidden md:flex">
+            <Link to='/'
+              className={`${ location.pathname.includes('/dashboard') && 'border-b-3 border-[#02A9EB]' } text-sm
+              hover:text-gray-600`}>
+              Dashboard
+            </Link>
+            {/* <Link to='/' className="text-sm hover:text-gray-600">Job Application Tracker</Link> */}
+            <Link to='/' className="text-sm hover:text-gray-600">AI Resume Cheker</Link>
+            <Link to='/' className="text-sm hover:text-gray-600">Job Description Matching</Link>
+            <Link to='/' className="text-sm hover:text-gray-600">Cover Letter Generator</Link>
+        </div>
+
+        {/* Profile */}
+        <div>
+          <p className='flex items-center justify-center text-2xl md:text-4xl text-white w-12 h-12
+            rounded-full bg-[#02A9EB]'>
+            { user.username[0] }
+          </p>
+        </div>
+
+    </nav>
+    </>
+  )
+}
+
+export default UserNavbar
