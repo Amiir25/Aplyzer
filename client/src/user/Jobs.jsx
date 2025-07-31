950ab30a5299975c3a156cf13f378ec57ec26cd6
Amiir25 <amirsadik636@gmail.com>
2025-07-31 08:21:32 +0300

    Update Jobs.jsx

--- client/src/user/Jobs.jsx
+++ client/src/user/Jobs.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import { faCircleDot, faLocationDot } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import UserNavbar from './UserNavbar';
+import React, { useState } from 'react'
+import { faCircleDot, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as regularStar } from "@fortawesome/free-regular-svg-icons";
+
 const Jobs = () => {
 
     const Jobs = [
         {
+            id: 1,
             status: 'Applied',
             title: 'Intern Deeveloper',
             company: 'Afroel Tech',
@@ -16,6 +19,7 @@ const Jobs = () => {
             deadline: '01/Aug/2025'
         },
         {
+            id: 2,
             status: 'Interviwed',
             title: 'Intern Deeveloper',
             company: 'Afroel Tech',
@@ -26,6 +30,7 @@ const Jobs = () => {
             deadline: '01/Aug/2025'
         },
         {
+            id: 3,
             status: 'Applied',
             title: 'Intern Deeveloper',
             company: 'Afroel Tech',
@@ -36,6 +41,7 @@ const Jobs = () => {
             deadline: '01/Aug/2025'
         },
         {
+            id: 4,
             status: 'Interviwed',
             title: 'Intern Deeveloper',
             company: 'Afroel Tech',
@@ -46,6 +52,7 @@ const Jobs = () => {
             deadline: '01/Aug/2025'
         },
         {
+            id: 5,
             status: 'Rejected',
             title: 'Intern Deeveloper',
             company: 'Afroel Tech',
@@ -56,6 +63,7 @@ const Jobs = () => {
             deadline: '01/Aug/2025'
         },
         {
+            id: 6,
             status: 'Applied',
             title: 'Intern Deeveloper',
             company: 'Afroel Tech',
@@ -66,6 +74,7 @@ const Jobs = () => {
             deadline: '01/Aug/2025'
         },
         {
+            id: 7,
             status: 'Applied',
             title: 'Intern Deeveloper',
             company: 'Afroel Tech',
@@ -76,6 +85,7 @@ const Jobs = () => {
             deadline: '01/Aug/2025'
         },
         {
+            id: 8,
             status: 'Applied',
             title: 'Intern Deeveloper',
             company: 'Afroel Tech',
@@ -86,6 +96,7 @@ const Jobs = () => {
             deadline: '01/Aug/2025'
         },
         {
+            id: 9,
             status: 'Rejected',
             title: 'Intern Deeveloper',
             company: 'Afroel Tech',
@@ -96,6 +107,7 @@ const Jobs = () => {
             deadline: '01/Aug/2025'
         },
         {
+            id: 10,
             status: 'Interviwed',
             title: 'Intern Deeveloper',
             company: 'Afroel Tech',
@@ -106,65 +118,89 @@ const Jobs = () => {
             deadline: '01/Aug/2025'
         },
     ];
+
+    const [favourite, setFavourite] = useState({});
+
+    const toogleFavorite = (id) => {
+        setFavourite((prev) => ({
+            ...prev,
+            [id]: !prev[id]
+        }))
+    }
+
     return (
         <>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+        <div className='px-6 md:px-12 lg:px-24 xl:px-32 my-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
             {
-                Jobs.map((job, i) => (
-                    <div className='relative w-80 mt-10 pt-10 pb-2 px-4 shadow-lg rounded border border-gray-200'>
-                        {/* Status */}
-                        <p 
-                        className={`absolute -top-0.5 -right-0.5 text-lg text-gray-100 font-medium px-2 py-1
-                            ${ job.status === 'Applied' && 'bg-blue' }
-                            ${ job.status === 'Interviwed' && 'bg-green' }
-                            ${ job.status === 'Rejected' && 'bg-red' }`} >
-                            { job.status }
-                        </p>
-
-                        <h1 className='text-2xl md:text-3xl font-bold'>{ job.title }</h1>
+                Jobs.map((job, i) => {
+                    
+                    return (
+                        <div key={ job.id }
+                        className='relative w-80 mt-10 pt-10 pb-2 px-4 shadow-lg rounded border border-gray-200'>
                         
-                        {/* --- */}
-                        <div className='flex items-center justify-between'>
-                            <div className='text-gray-700'>
-                                <p className='text-lg md:text-xl'>{ job.company }</p>
-                                <p className='text-sm'>
-                                    <FontAwesomeIcon icon={ faLocationDot } className='me-1' />
-                                    { job.location }
+                            {/* Status */}
+                            <p 
+                            className={`absolute -top-0.5 -left-0.5 text-lg text-gray-100 font-medium px-2 py-1
+                                ${ job.status === 'Applied' && 'bg-blue' }
+                                ${ job.status === 'Interviwed' && 'bg-green' }
+                                ${ job.status === 'Rejected' && 'bg-red' }`} >
+                                { job.status }
+                            </p>
+
+                            {/* Star */}
+                            <button
+                            onClick={ () => toogleFavorite(job.id) }
+                            className={`absolute top-1 right-1 text-lg text-gray-800 font-medium`}>
+                                <FontAwesomeIcon icon={ favourite[job.id] ? solidStar : regularStar }
+                                className={`${ favourite[job.id] && 'text-amber-500' }`} />
+                            </button>
+
+                            <h1 className='text-2xl md:text-3xl font-bold'>{ job.title }</h1>
+                            
+                            {/* --- */}
+                            <div className='flex items-center justify-between'>
+                                <div className='text-gray-700'>
+                                    <p className='text-lg md:text-xl'>{ job.company }</p>
+                                    <p className='text-sm'>
+                                        <FontAwesomeIcon icon={ faLocationDot } className='me-1' />
+                                        { job.location }
+                                    </p>
+                                </div>
+                                <p className='text-xs bg-gray-200 px-2 py-1 text-fuchsia-800 rounded-full'>
+                                    <FontAwesomeIcon icon={ faCircleDot } className='me-1' />
+                                    { job.type } / { job.mode }
                                 </p>
                             </div>
-                            <p className='text-xs bg-gray-200 px-2 py-1 text-fuchsia-800 rounded-full'>
-                                <FontAwesomeIcon icon={ faCircleDot } className='me-1' />
-                                { job.type } / { job.mode }
-                            </p>
-                        </div>
 
-                        {/* Dates */}
-                        <div className='grid grid-cols-2 gap-10 text-sm my-6 border-t border-b border-gray-300
-                            py-3'>
-                            <div>
-                                <p>Applied Date</p>
-                                <p className='text-xs'>{ job.applied }</p>
+                            {/* Dates */}
+                            <div className='grid grid-cols-2 gap-10 text-sm my-6 border-t border-b border-gray-300
+                                py-3'>
+                                <div>
+                                    <p>Applied Date</p>
+                                    <p className='text-xs'>{ job.applied }</p>
+                                </div>
+                                <div>
+                                    <p>Deadline</p>
+                                    <p className='text-xs text-red-700'>{ job.deadline }</p>
+                                </div>
                             </div>
-                            <div>
-                                <p>Deadline</p>
-                                <p className='text-xs text-red-700'>{ job.deadline }</p>
-                            </div>
-                        </div>
 
-                        {/* Buttons */}
-                        <div className='flex items-center justify-around gap-4 mt-8'>
-                            <button className='px-3 py-1 w-1/3 rounded border border-blue-500 hover:text-white hover:bg-blue-500'>
-                                View
-                            </button>
-                            <button className='px-3 py-1 w-1/3 rounded border border-green-500 hover:text-white hover:bg-green-500'>
-                                Update
-                            </button>
-                            <button className='px-3 py-1 w-1/3 rounded border border-red-500 hover:text-white hover:bg-red-500'>
-                                Delete
-                            </button>
+                            {/* Buttons */}
+                            <div className='flex items-center justify-around gap-4 mt-8'>
+                                <button className='px-3 py-1 w-1/3 rounded border border-blue-500 hover:text-white hover:bg-blue-500'>
+                                    View
+                                </button>
+                                <button className='px-3 py-1 w-1/3 rounded border border-green-500 hover:text-white hover:bg-green-500'>
+                                    Update
+                                </button>
+                                <button className='px-3 py-1 w-1/3 rounded border border-red-500 hover:text-white hover:bg-red-500'>
+                                    Delete
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                ))
+                    )
+
+                })
             }
         </div>
         </>
