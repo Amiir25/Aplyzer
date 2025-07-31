861b75bb19d6047cfe6719d029d4b7e6df947741
Amiir25 <amirsadik636@gmail.com>
2025-07-31 11:39:23 +0300

    Add AddNewJob.jsx

---
+++ client/src/user/AddNewJob.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AddNewJob = () => {
+  return (
+    <>
+    <div className='px-6 md:px-12 lg:px-24 xl:px-32 my-20'>
+
+        <h1 className='text-2xl md:text-4xl font-semibold mb-10'>Add a new job</h1>
+
+        <form action="">
+
+        </form>
+
+    </div>
+    </>
+  )
+}
+
+export default AddNewJob
