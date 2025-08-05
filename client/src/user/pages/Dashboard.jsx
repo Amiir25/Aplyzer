b14b4adeb89d73ee3dc02dd10f58e407ed895e61
Amiir25 <amirsadik636@gmail.com>
2025-08-05 19:52:51 +0300

    Create a separate components and pages folders for user inside user forlder

---
+++ client/src/user/pages/Dashboard.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import { Route, Routes, useParams } from 'react-router-dom'
+import axios from 'axios';
+import UserNavbar from '../components/UserNavbar';
+import StatusSummery from '../components/StatusSummery';
+import StatusBreakdown from '../components/StatusBreakdown';
+import LastWeek from '../components/LastWeek';
+import GetReady from '../components/GetReady';
+import RecentApplications from '../components/RecentApplications';
+import QuickActions from '../components/QuickActions';
+
+const Dashboard = () => {
+    const { userId } = useParams();
+    const [userData, setUserData] = useState({});
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3000/user/dashboard/${userId}`);
+                setUserData(response.data.user);
+                console.log('Dashboard response', response.data.user);
+            } catch (error) {
+                const errorMsg = error.response?.data?.message;
+            }
+        }
+        fetchUserData();
+    }, [userId]);
+
+    return (
+        <>
+            <div>
+                <UserNavbar userData={userData} />
+                {/* {alert(JSON.stringify(userData))} */}
+
+
+                <div className='px-6 md:px-12 lg:px-24 xl:px-32 mt-20 mb-40'>
+
+                    <h1 className='text-2xl md:text-3xl'>
+                        Hi <span className='font-mono font-bold text-shadow-lg'>{userData.username}</span>
+                    </h1>
+                    <p className='text-lg md:text-xl text-gray-600'>Here's a quick look at your job applications </p>
+                </div>
+
+                <StatusSummery userData={userData} />
+                <LastWeek userData={userData} />
+                <GetReady userData={userData} />
+                <RecentApplications userData={userData} />
+                <StatusBreakdown userData={userData} />
+                <QuickActions userData={userData} />
+
+            </div>
+        </>
+    )
+}
+
+export default Dashboard
