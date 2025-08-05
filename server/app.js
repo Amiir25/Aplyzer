14ae953e0b781e952cb0487da6f06e7c2bc7332f
Amiir25 <amirsadik636@gmail.com>
2025-08-05 19:55:10 +0300

    Add jobsRouter in app.js

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 
 import authRouter from './routes/authRouter.js';
 import userRouter from './routes/userRouter.js';
+import jobsRouter from './routes/jobsRouter.js';
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,7 @@ app.use(cors());
 // Routes
 app.use('/auth', authRouter);
 app.use('/user', userRouter);
+app.use('/jobs', jobsRouter);
 
 const port = process.env.PORT;
 app.listen(port, () => {
