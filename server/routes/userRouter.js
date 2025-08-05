25c3045961ec7262ccde1ca9646a2b1ee2947c4f
Amiir25 <amirsadik636@gmail.com>
2025-08-05 19:57:57 +0300

    Update dashboard route in userRouter.js

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,7 +3,7 @@ import { userDashboard, userProfile } from "../controllers/userController.js";
 
 const userRouter = Router();
 
-userRouter.get('/dashboard/:id/', userDashboard);
+userRouter.get('/dashboard/:id', userDashboard);
 userRouter.get('/profile/:id', userProfile);
 
 export default userRouter;
