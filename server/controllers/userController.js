edd997468fdec0a17f3ecca7695078c4a4d0f80c
Amiir25 <amirsadik636@gmail.com>
2025-08-12 18:15:37 +0300

    Update userDashboard function to use Promise to run multiple queries in userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,39 +1,30 @@
 import db from "../database/db.js";
 
-export const userDashboard = (req, res, next) => {
+export const userDashboard = async (req, res) => {
     const { id } = req.params;
 
-    const userQuery = 'SELECT username FROM users WHERE uid = ?';
-    const allJobsQuery = 'SELECT * FROM jobs WHERE user_id  = ?';
-    const recentJobsQuery = 'SELECT * FROM jobs WHERE user_id = ? AND applied_date >= CURDATE() - INTERVAL 3 DAY';
+    try {
+        // Run multiple queries in parallel
+        const [user, jobs, recentJobs] = await Promise.all([
+            db.query('SELECT username FROM users WHERE uid = ?', [id]),
+            db.query('SELECT * FROM jobs WHERE user_id  = ?', [id]),
+            db.query('SELECT * FROM jobs WHERE user_id = ? AND applied_date >= CURDATE() - INTERVAL 3 DAY', [id])
+        ]);
 
-    db.query(userQuery, id, (err, usernames) => {
-        if (err) {
-            console.error('DB error', err);
-            return res.status(500).json({ message: 'Database error' });
+        if (!user.length) {
+            return res.status(404).json({ message: 'User not found' })
         }
 
-            db.query(allJobsQuery, id, (err, jobs) => {
-                if (err) {
-                    console.error('DB error', err);
-                    return res.status(500).json({ message: 'Database error' });
-                }
-
-                db.query(recentJobsQuery, id, (err, recentJobs) => {
-                    if (err) {
-                        console.error('DB error', err);
-                        return res.status(500).json({ message: 'Database error' });
-                    }
-                    
-                    if (!usernames.length) return res.status(409).json({ message: 'User not found, Try again' });
-                    return res.status(200).json({
-                        username: usernames[0].username,
-                        jobs: jobs,
-                        recentJobs: recentJobs
-                    });
-                })
-            });
-    })
+        return res.status(200).json({
+            username: user[0].username,
+            jobs,
+            recentJobs
+        });
+    
+    } catch (err) {
+        console.error('DB error', err);
+        return res.status(500).json({ message: 'Database error' });
+    }
 }
 
 // User profile
