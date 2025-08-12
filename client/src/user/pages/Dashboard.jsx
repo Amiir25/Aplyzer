13f874dd6e1f8d0d0746d22f23cda84618cfbcce
Amiir25 <amirsadik636@gmail.com>
2025-08-12 17:58:07 +0300

    Handle multiple responses from the backend Dashboard.jsx

--- client/src/user/pages/Dashboard.jsx
+++ client/src/user/pages/Dashboard.jsx
@@ -11,13 +11,18 @@ import QuickActions from '../components/QuickActions';
 
 const Dashboard = () => {
     const { userId } = useParams();
-    const [jobs, setJobs] = useState({});
+
+    const [username, setUsername] = useState();
+    const [jobs, setJobs] = useState([]);
+    const [recentJobs, setRecentJobs] = useState([]);
 
     useEffect(() => {
         const fetchUserData = async () => {
             try {
                 const response = await axios.get(`http://localhost:3000/user/dashboard/${userId}`);
+                setUsername(response.data.username);
                 setJobs(response.data.jobs);
+                setRecentJobs(response.data.recentJobs);
                 // console.log('Dashboard response', response.data.jobs);
             } catch (error) {
                 const errorMsg = error.response?.data?.message;
@@ -25,8 +30,6 @@ const Dashboard = () => {
         }
         fetchUserData();
     }, [userId]);
-
-    console.log(jobs[1]);
 
     return (
         <>
@@ -36,16 +39,16 @@ const Dashboard = () => {
                 <div className='px-6 md:px-12 lg:px-24 xl:px-32 mt-20 mb-40'>
 
                     <h1 className='text-2xl md:text-3xl'>
-                        {/* Hi <span className='font-mono font-bold text-shadow-lg'>{jobs.job_title }</span> */}
+                        Hi <span className='font-mono font-bold text-shadow-lg'>{ username }</span>
                     </h1>
                     <p className='text-lg md:text-xl text-gray-600'>Here's a quick look at your job applications </p>
                 </div>
 
-                {/* <StatusSummery jobs={jobs} />
-                <LastWeek jobs={jobs} />
-                <GetReady jobs={jobs} />
+                <StatusSummery jobs={jobs} />
+                {/* <LastWeek jobs={jobs} /> */}
+                {/* <GetReady jobs={jobs} /> */}
                 <RecentApplications jobs={jobs} />
-                <StatusBreakdown jobs={jobs} />
+                {/*<StatusBreakdown jobs={jobs} />
                 <QuickActions jobs={jobs} /> */}
 
             </div>
