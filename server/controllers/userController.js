c8984a14c7b9fd06faff8c7eaed136a8eca7ad79
Amiir25 <amirsadik636@gmail.com>
2025-08-12 17:57:06 +0300

    Update userDashboard function to hanlde multiple queries in userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,17 +2,38 @@ import db from "../database/db.js";
 
 export const userDashboard = (req, res, next) => {
     const { id } = req.params;
-    const fetchQuery = 'SELECT * FROM users WHERE uid = ?';
 
-    db.query(fetchQuery, id, (err, users) => {
+    const userQuery = 'SELECT username FROM users WHERE uid = ?';
+    const allJobsQuery = 'SELECT * FROM jobs WHERE user_id  = ?';
+    const recentJobsQuery = 'SELECT * FROM jobs WHERE user_id = ? AND applied_date >= CURDATE() - INTERVAL 3 DAY';
+
+    db.query(userQuery, id, (err, usernames) => {
         if (err) {
             console.error('DB error', err);
             return res.status(500).json({ message: 'Database error' });
         }
 
-        if (!users.length) return res.status(409).json({ message: 'User not found, Try again' });
-        return res.status(200).json({ user: users[0] });
-    });
+            db.query(allJobsQuery, id, (err, jobs) => {
+                if (err) {
+                    console.error('DB error', err);
+                    return res.status(500).json({ message: 'Database error' });
+                }
+
+                db.query(recentJobsQuery, id, (err, recentJobs) => {
+                    if (err) {
+                        console.error('DB error', err);
+                        return res.status(500).json({ message: 'Database error' });
+                    }
+                    
+                    if (!usernames.length) return res.status(409).json({ message: 'User not found, Try again' });
+                    return res.status(200).json({
+                        username: usernames[0].username,
+                        jobs: jobs,
+                        recentJobs: recentJobs
+                    });
+                })
+            });
+    })
 }
 
 // User profile
