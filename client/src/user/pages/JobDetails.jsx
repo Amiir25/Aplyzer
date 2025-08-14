3f9c93d9bfcdb155a9993b54f461e67fd7cf1a7f
Amiir25 <amirsadik636@gmail.com>
2025-08-14 18:18:25 +0300

    Add UserNavbar in JobDetails.jsx

---
+++ client/src/user/pages/JobDetails.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import UserNavbar from '../components/UserNavbar'
+
+const JobDetails = () => {
+  return (
+    <>
+    <div>
+        
+        <UserNavbar/>
+
+        {/* Job Details */}
+
+    </div>
+    </>
+  )
+}
+
+export default JobDetails
