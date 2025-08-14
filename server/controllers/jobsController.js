fe24661b83a7aa95472305fea6d6701ed4869d2a
Amiir25 <amirsadik636@gmail.com>
2025-08-14 18:01:09 +0300

    Update jobsController.jsx

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -2,6 +2,24 @@ import db from "../database/db.js";
 
 export const getAllJobs = (req, res) => {
     const { id } = req.params;
+    try {
+        const fetchQuery = 'SELECT * FROM jobs WHERE user_id = ?';
+        db.query(fetchQuery, [id], (err, jobs) => {
+            if (err) {
+                console.log('Database Error:', err);
+                return res.status(500).json({ message: 'Database error' });
+            }
+
+            if (!jobs.length) {
+                return res.status(400).json({ message: 'User has no saved jobs yet' });
+            }
+
+            return res.status(200).json({ jobs });
+        })
+    } catch (error) {
+        console.log('Server error:', error)
+        return res.status(500).json({ message: 'Internal server error' });
+    }
 
 }
 
