365aa1d0756c6145a973d4f170d3d570d5d4374a
Amiir25 <amirsadik636@gmail.com>
2025-08-14 18:09:32 +0300

    Rename getJob function to getJobDetails in userRouter.js

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { userDashboard, userProfile } from "../controllers/userController.js";
-import { getAllJobs, getJob } from "../controllers/jobsController.js";
+import { getAllJobs, getJobDetails } from "../controllers/jobsController.js";
 
 const userRouter = Router();
 
 userRouter.get('/dashboard/:id', userDashboard);
 userRouter.get('/profile/:id', userProfile);
 userRouter.get('/all-jobs/:id', getAllJobs);
-userRouter.get('/job-details/:id', getJob);
+userRouter.get('/job-details/:id', getJobDetails);
 
 
 
