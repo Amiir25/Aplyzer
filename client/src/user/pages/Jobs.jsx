d569a748e647c519dbaeb76e83d1cecb38a52891
Amiir25 <amirsadik636@gmail.com>
2025-08-14 15:28:34 +0300

    Remove the absolute position of the experiance level element in Jobs.jsx

--- client/src/user/pages/Jobs.jsx
+++ client/src/user/pages/Jobs.jsx
@@ -173,8 +173,8 @@ const Jobs = () => {
                                 {/* Status */}
                                 <p className={`absolute -top-0.5 -left-0.5 text-lg text-gray-100 font-medium px-2 py-1
                                 ${job.status === 'Applied' && 'bg-blue'}
-                                ${job.status === 'Interviwed' && 'bg-green'}
-                                ${job.status === 'Rejected' && 'bg-red'}`} >
+                                ${job.status === 'Interviewed' && 'bg-purple'}
+                                ${job.status === 'Hired' && 'bg-green'}`} >
                                     {job.status}
                                 </p>
 
