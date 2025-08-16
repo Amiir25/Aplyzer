70fbc1ea87cfb631d9aca2f611bc0458f2da7b4e
Amiir25 <amirsadik636@gmail.com>
2025-08-16 22:07:57 +0300

    Create a function to fetch job details from the db in JobDetails.jsx

--- client/src/user/pages/JobDetails.jsx
+++ client/src/user/pages/JobDetails.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
 import UserNavbar from '../components/UserNavbar'
+import axios from 'axios';
 
 const JobDetails = () => {
+
+  const { jobId } = useParams();
+
+  // Fetch job details
+  useEffect(() => {
+    const fetchJobDetails = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3000/user/job-details/${jobId}`);
+        console.log(response);
+      } catch (error) {
+        const errorMsg = error.response?.data?.message;
+      }
+    }
+    fetchJobDetails();
+  }, [jobId]);
+
   return (
     <>
     <div>
@@ -10,6 +27,13 @@ const JobDetails = () => {
         <UserNavbar/>
 
         {/* Job Details */}
+        <div>
+
+          {/* --- */}
+          <div>
+            { jobId }
+          </div>
+        </div>
 
     </div>
     </>
