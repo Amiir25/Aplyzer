44136952e55dbc0bf0d8e905a0f1ebc722e3d06b
Amiir25 <amirsadik636@gmail.com>
2025-08-16 22:23:29 +0300

    Create a state variable to store the job details in JobDetails.jsx

--- client/src/user/pages/JobDetails.jsx
+++ client/src/user/pages/JobDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import UserNavbar from '../components/UserNavbar'
 import axios from 'axios';
@@ -6,13 +6,14 @@ import axios from 'axios';
 const JobDetails = () => {
 
   const { jobId } = useParams();
+  const [job, setJob] = useState({});
 
   // Fetch job details
   useEffect(() => {
     const fetchJobDetails = async () => {
       try {
         const response = await axios.get(`http://localhost:3000/user/job-details/${jobId}`);
-        console.log(response);
+        setJob(response.data.job);
       } catch (error) {
         const errorMsg = error.response?.data?.message;
       }
