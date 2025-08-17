1e6ce0125c0802d86348ca72b1b4945a0fed1b23
Amiir25 <amirsadik636@gmail.com>
2025-08-17 18:25:28 +0300

    Restructure the input fileds in AddNewJob.jsx

--- client/src/user/pages/AddNewJob.jsx
+++ client/src/user/pages/AddNewJob.jsx
@@ -3,6 +3,9 @@ import UserNavbar from '../components/UserNavbar';
 import * as yup from 'yup';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAlignLeft, faCalendarCheck, faSuitcase, faSuitcaseMedical, faSuitcaseRolling } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 
 const AddNewJob = () => {
 
@@ -38,6 +41,9 @@ const AddNewJob = () => {
     deadline: yup
       .string()
       .required('Application deadline is required'),
+    status: yup
+      .string()
+      .required('Status is required'),
     postLink: yup
       .string()
       .required('Job post link is required'),
@@ -63,137 +69,184 @@ const AddNewJob = () => {
     <UserNavbar/>
 
     <div className='px-6 md:px-12 lg:px-24 xl:px-32 my-20'>
-      <h1 className='text-2xl md:text-4xl font-semibold mb-10'>Add New Job Application</h1>
+      <h1 className='text-2xl md:text-5xl text-center font-bold mb-10'>Capture New Job Opportunity</h1>
 
       <form onSubmit={ handleSubmit(onSubmit) }>
         
-        <div className='grid grid-col-1 md:grid-cols-2 gap-x-20'>
-          {/* Job title */}
-          <div className='mb-4'>
-            <label htmlFor="jobTitle" className='block text-lg md:text-2xl text-gray-800'>Job Title</label>
-            <input id='jobTitle' type="text" placeholder='e.g. Software Engineer'
-            className='px-3 py-2 border border-gray-400 rounded w-full'
-            { ...register('jobTitle') } />
-            { errors.jobTitle && <p className='text-sm text-red-600'>{ errors.jobTitle.message }</p> }
-          </div>
-          {/* Company */}
-          <div className='mb-4'>
-            <label htmlFor="company" className='block text-lg md:text-2xl text-gray-800'>Company</label>
-            <input id='company' type="text" placeholder='e.g. Google'
-            className='px-3 py-2 border border-gray-400 rounded w-full'
-            { ...register('company') } />
-            { errors.company && <p className='text-sm text-red-600'>{ errors.company.message }</p> }
-          </div>
-          {/* Location */}
-          <div className='mb-4'>
-            <label htmlFor="location" className='block text-lg md:text-2xl text-gray-800'>Location</label>
-            <input id='location' type="text" placeholder='e.g. Addis Ababa, Ethiopia'
-            className='px-3 py-2 border border-gray-400 rounded w-full'
-            { ...register('location') } />
-            { errors.location && <p className='text-sm text-red-600'>{ errors.location.message }</p> }
-          </div>
-          {/* Job type */}
-          <div className='mb-4'>
-            <label htmlFor="jobType" className='block text-lg md:text-2xl text-gray-800'>Job Type</label>
-            <input id='jobType' type="text" list='jobTypeList' placeholder='Select job type'
-            className='px-3 py-2 border border-gray-400 rounded w-full'
-            { ...register('jobType') } />
-            <datalist id='jobTypeList'>
-              <option value="Full-time"></option>
-              <option value="Part-time"></option>
-              <option value="Internship"></option>
-              <option value="Contractual"></option>
-              <option value="Unknown"></option>
-            </datalist>
-            { errors.jobType && <p className='text-sm text-red-600'>{ errors.jobType.message }</p> }
-          </div>
-          {/* Work Mode */}
-          <div className='mb-4'>
-            <label htmlFor="workMode" className='block text-lg md:text-2xl text-gray-800'>Work Mode</label>
-            <input id='workMode' type="text" list='workModeList' placeholder='Select work mode'
-            className='px-3 py-2 border border-gray-400 rounded w-full'
-            { ...register('workMode') } />
-            <datalist id='workModeList'>
-              <option value="Onsite"></option>
-              <option value="Remote"></option>
-              <option value="Hybrid"></option>
-              <option value="Unknown"></option>
-            </datalist>
-            { errors.workMode && <p className='text-sm text-red-600'>{ errors.jobType.message }</p> }
-          </div>
-          {/* Experiance Level */}
-          <div className='mb-4'>
-            <label htmlFor="expLevel" className='block text-lg md:text-2xl text-gray-800'>Experiance Level</label>
-            <input id='expLevel' type="text" list='expLevelList' placeholder='Select experiance level'
-            className='px-3 py-2 border border-gray-400 rounded w-full'
-            { ...register('expLevel') } />
-            <datalist id='expLevelList'>
-              <option value="Onsite"></option>
-              <option value="Remote"></option>
-              <option value="Hybrid"></option>
-              <option value="Unknown"></option>
-            </datalist>
-            { errors.expLevel && <p className='text-sm text-red-600'>{ errors.jobType.message }</p> }
-          </div>
-          {/* Applied Date */}
-          <div className='mb-4'>
-            <label htmlFor="appliedDate" className='block text-lg md:text-2xl text-gray-800'>Applied Date</label>
-            <input id='appliedDate' type="date"
-            className='px-3 py-2 border border-gray-400 rounded w-full'
-            { ...register('appliedDate') } />
-            { errors.appliedDate && <p className='text-sm text-red-600'>{ errors.appliedDate.message }</p> }
-          </div>
-          {/* Deadline */}
-          <div className='mb-4'>
-            <label htmlFor="deadline" className='block text-lg md:text-2xl text-gray-800'>Deadline</label>
-            <input id='deadline' type="date"
-            className='px-3 py-2 border border-gray-400 rounded w-full'
-            { ...register('deadline') } />
-            { errors.deadline && <p className='text-sm text-red-600'>{ errors.deadline.message }</p> }
-          </div>
-          {/* Job post link */}
-          <div className='mb-4'>
-            <label htmlFor="postLink" className='block text-lg md:text-2xl text-gray-800'>Job Post Link</label>
-            <input id='postLink' type="text" placeholder='e.g. Addis Ababa, Ethiopia'
-            className='px-3 py-2 border border-gray-400 rounded w-full'
-            { ...register('postLink') } />
-            { errors.postLink && <p className='text-sm text-red-600'>{ errors.postLink.message }</p> }
-          </div>
-          {/* More Information / Notes */}
-          <div className='mb-4'>
-            <label htmlFor="moreInfo" className='block text-lg md:text-2xl text-gray-800'>More Information / Notes</label>
-            <textarea id="moreInfo" rows="1" placeholder="Add any additional notes here"
-            className='px-3 py-2 border border-gray-400 rounded w-full'
-            { ...register('moreInfo') }></textarea>
-            { errors.moreInfo && <p className='text-sm text-red-600'>{ errors.moreInfo.message }</p> }
-          </div>
-          {/* Required Skills */}
-          <div className='mb-4'>
-            <label htmlFor="reqSkills" className='block text-lg md:text-2xl text-gray-800'>Required Skills</label>
-            <textarea id="reqSkills" rows="6" placeholder="List skills, one per line or comma-separated"
-            className='px-3 py-2 border border-gray-400 rounded w-full'
-            { ...register('reqSkills') }></textarea>
-            { errors.reqSkills && <p className='text-sm text-red-600'>{ errors.reqSkills.message }</p> }
-          </div>
-          {/* Job Description */}
-          <div className='mb-4'>
-            <label htmlFor="jobDesc" className='block text-lg md:text-2xl text-gray-800'>Job Description</label>
-            <textarea id="jobDesc" rows="6" placeholder="Paste the full job description here..."
-            className='px-3 py-2 border border-gray-400 rounded w-full'
-            { ...register('jobDesc') }></textarea>
-            { errors.jobDesc && <p className='text-sm text-red-600'>{ errors.jobDesc.message }</p> }
-          </div>
-          {/* Favorite */}
-          <div className='mb-4'>
-            <input type="checkbox" id='favorite'
-            className='px-3 py-2 border border-gray-400 rounded mx-2'
-            { ...register('favorite') } />
-            <label htmlFor="favorite" className='text-lg md:text-2xl text-gray-800'>Mark As Favorite</label>
-            { errors.favorite && <p className='text-sm text-red-600'>{ errors.favorite.message }</p> }
-          </div>
-        
-        </div>
+        {/* Job Essentials */}
+        <section className='px-10 py-4 my-10 bg-sky-50 rounded-xl'>
+          <h1 className='text-2xl font-medium mb-4'>
+            <FontAwesomeIcon icon={ faSuitcase } className='text-purple-500' />
+            <span> Job Essentials</span>
+          </h1>
 
+          <div className='grid grid-col-1 md:grid-cols-2 gap-x-20'>
+            {/* Job title */}
+            <div className='mb-4'>
+              <label htmlFor="jobTitle" className='block text-xl text-gray-800'>Job Title</label>
+              <input id='jobTitle' type="text" placeholder='e.g. Software Engineer'
+              className='px-3 py-2 border border-gray-400 rounded w-full'
+              { ...register('jobTitle') } />
+              { errors.jobTitle && <p className='text-sm text-red-600'>{ errors.jobTitle.message }</p> }
+            </div>
+            {/* Company */}
+            <div className='mb-4'>
+              <label htmlFor="company" className='block text-xl text-gray-800'>Company</label>
+              <input id='company' type="text" placeholder='e.g. Google'
+              className='px-3 py-2 border border-gray-400 rounded w-full'
+              { ...register('company') } />
+              { errors.company && <p className='text-sm text-red-600'>{ errors.company.message }</p> }
+            </div>
+            {/* Location */}
+            <div className='mb-4'>
+              <label htmlFor="location" className='block text-xl text-gray-800'>Location</label>
+              <input id='location' type="text" placeholder='e.g. Addis Ababa, Ethiopia'
+              className='px-3 py-2 border border-gray-400 rounded w-full'
+              { ...register('location') } />
+              { errors.location && <p className='text-sm text-red-600'>{ errors.location.message }</p> }
+            </div>
+            {/* Job type */}
+            <div className='mb-4'>
+              <label htmlFor="jobType" className='block text-xl text-gray-800'>Job Type</label>
+              <input id='jobType' type="text" list='jobTypeList' placeholder='Select job type'
+              className='px-3 py-2 border border-gray-400 rounded w-full'
+              { ...register('jobType') } />
+              <datalist id='jobTypeList'>
+                <option value="Full-time"></option>
+                <option value="Part-time"></option>
+                <option value="Internship"></option>
+                <option value="Contractual"></option>
+                <option value="Unknown"></option>
+              </datalist>
+              { errors.jobType && <p className='text-sm text-red-600'>{ errors.jobType.message }</p> }
+            </div>
+            {/* Work Mode */}
+            <div className='mb-4'>
+              <label htmlFor="workMode" className='block text-xl text-gray-800'>Work Mode</label>
+              <input id='workMode' type="text" list='workModeList' placeholder='Select work mode'
+              className='px-3 py-2 border border-gray-400 rounded w-full'
+              { ...register('workMode') } />
+              <datalist id='workModeList'>
+                <option value="Onsite"></option>
+                <option value="Remote"></option>
+                <option value="Hybrid"></option>
+                <option value="Unknown"></option>
+              </datalist>
+              { errors.workMode && <p className='text-sm text-red-600'>{ errors.workMode.message }</p> }
+            </div>
+            {/* Experiance Level */}
+            <div className='mb-4'>
+              <label htmlFor="status" className='block text-xl text-gray-800'>Experiance Level</label>
+              <input id='status' type="text" list='statusList' placeholder='Select experiance level'
+              className='px-3 py-2 border border-gray-400 rounded w-full'
+              { ...register('status') } />
+              <datalist id='statusList'>
+                <option value="Onsite"></option>
+                <option value="Remote"></option>
+                <option value="Hybrid"></option>
+                <option value="Unknown"></option>
+              </datalist>
+              { errors.status && <p className='text-sm text-red-600'>{ errors.status.message }</p> }
+            </div>
+          </div>
+        </section>
+
+        {/* Timeline & Status */}
+        <section className='px-10 py-4 my-10 bg-sky-50 rounded-xl'>
+          <h1 className='text-2xl font-medium mb-4'>
+            <FontAwesomeIcon icon={ faCalendarCheck } className='text-purple-500' />
+            <span> Timeline & Status</span>
+          </h1>
+          
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20'>
+            {/* Applied Date */}
+            <div className='mb-4'>
+              <label htmlFor="appliedDate" className='block text-xl text-gray-800'>Applied Date</label>
+              <input id='appliedDate' type="date"
+              className='px-3 py-2 border border-gray-400 rounded w-full'
+              { ...register('appliedDate') } />
+              { errors.appliedDate && <p className='text-sm text-red-600'>{ errors.appliedDate.message }</p> }
+            </div>
+            {/* Deadline */}
+            <div className='mb-4'>
+              <label htmlFor="deadline" className='block text-xl text-gray-800'>Deadline</label>
+              <input id='deadline' type="date"
+              className='px-3 py-2 border border-gray-400 rounded w-full'
+              { ...register('deadline') } />
+              { errors.deadline && <p className='text-sm text-red-600'>{ errors.deadline.message }</p> }
+            </div>
+            {/* Status */}
+            <div className='mb-4'>
+              <label htmlFor="status" className='block text-xl text-gray-800'>Status</label>
+              <input id='status' type="text" list='statusList' placeholder='Select the current job status'
+              className='px-3 py-2 border border-gray-400 rounded w-full'
+              { ...register('status') } />
+              <datalist id='statusList'>
+                <option value="Applied"></option>
+                <option value="Waiting"></option>
+                <option value="Interviewed"></option>
+                <option value="Hired"></option>
+                <option value="Rejected"></option>
+                <option value="Quit"></option>
+              </datalist>
+              { errors.status && <p className='text-sm text-red-600'>{ errors.status.message }</p> }
+            </div>
+          </div>
+        </section>
+
+        {/* Description & Notes */}
+        <section className='px-10 py-4 my-10 bg-sky-50 rounded-xl'>
+          <h1 className='text-2xl font-medium mb-4'>
+            <FontAwesomeIcon icon={ faAlignLeft } className='text-purple-500' />
+            <span> Description & Notes</span>
+          </h1>
+          
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-x-20'>
+            {/* Job Description */}
+            <div className='mb-4'>
+              <label htmlFor="jobDesc" className='block text-xl text-gray-800'>Job Description</label>
+              <textarea id="jobDesc" rows="6" placeholder="Paste the full job description here..."
+              className='px-3 py-2 border border-gray-400 rounded w-full'
+              { ...register('jobDesc') }></textarea>
+              { errors.jobDesc && <p className='text-sm text-red-600'>{ errors.jobDesc.message }</p> }
+            </div>
+            {/* Required Skills */}
+            <div className='mb-4'>
+              <label htmlFor="reqSkills" className='block text-xl text-gray-800'>Required Skills</label>
+              <textarea id="reqSkills" rows="6" placeholder="List skills, one per line or comma-separated"
+              className='px-3 py-2 border border-gray-400 rounded w-full'
+              { ...register('reqSkills') }></textarea>
+              { errors.reqSkills && <p className='text-sm text-red-600'>{ errors.reqSkills.message }</p> }
+            </div>
+            {/* More Information / Notes */}
+            <div className='mb-4'>
+              <label htmlFor="moreInfo" className='block text-xl text-gray-800'>More Information / Notes</label>
+              <textarea id="moreInfo" rows="1" placeholder="Add any additional notes here"
+              className='px-3 py-2 border border-gray-400 rounded w-full'
+              { ...register('moreInfo') }></textarea>
+              { errors.moreInfo && <p className='text-sm text-red-600'>{ errors.moreInfo.message }</p> }
+            </div>
+            {/* Job post link */}
+            <div className='mb-4'>
+              <label htmlFor="postLink" className='block text-xl text-gray-800'>Job Post Link</label>
+              <input id='postLink' type="text" placeholder='e.g. Addis Ababa, Ethiopia'
+              className='px-3 py-2 border border-gray-400 rounded w-full'
+              { ...register('postLink') } />
+              { errors.postLink && <p className='text-sm text-red-600'>{ errors.postLink.message }</p> }
+            </div>
+          </div>
+        </section>
+
+        {/* Favorite */}
+        <div className='mb-4 text-center'>
+          <input type="checkbox" id='favorite'
+          className='px-3 py-2 border border-gray-400 rounded mx-2'
+          { ...register('favorite') } />
+          <label htmlFor="favorite" className='text-xl text-gray-800'>Mark as Favorite</label>
+          <FontAwesomeIcon icon={ faHeart } className='mx-2 text-red-500' />
+          { errors.favorite && <p className='text-sm text-red-600'>{ errors.favorite.message }</p> }
+        </div>
+        
         {/* Submit & Cancel Buttons */}
         <div className='flex items-center gap-4 justify-end'>
           {/* Submit */}
