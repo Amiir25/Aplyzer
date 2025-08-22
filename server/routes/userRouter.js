d5f6eefc5f323c80163dd756988ead5d0c89b84d
Amiir25 <amirsadik636@gmail.com>
2025-08-22 07:53:23 +0300

    Add new route for UpdateJob page in userRouter.js

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { userDashboard, userProfile } from "../controllers/userController.js";
-import { addNewJob, getAllJobs, getJobDetails } from "../controllers/jobsController.js";
+import { addNewJob, getAllJobs, getJobDetails, updateJob } from "../controllers/jobsController.js";
 
 const userRouter = Router();
 
@@ -9,7 +9,6 @@ userRouter.get('/profile/:id', userProfile);
 userRouter.get('/all-jobs/:id', getAllJobs);
 userRouter.get('/job-details/:id', getJobDetails);
 userRouter.post('/add-new-job/:id', addNewJob);
-
-
+userRouter.put('/user/update-job/:id', updateJob);
 
 export default userRouter;
