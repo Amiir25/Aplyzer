217da9e27b4ac7a6cb3ca3cfcaadf1889879ad99
Amiir25 <amirsadik636@gmail.com>
2025-08-25 18:37:42 +0300

    Modify the cards in SmarterTools.jsx

--- client/src/components/SmarterTools.jsx
+++ client/src/components/SmarterTools.jsx
@@ -17,16 +17,16 @@ const SmarterTools = () => {
             {
                 tools.map((tool, i) => (
                     <div key={ i }
-                    className='text-left mx-auto flex md:flex-col gap-10 md:gap-6 p-4 rounded
-                     hover:shadow-black hover:shadow-xl transition-all duration-400 ease-in-out'>
+                    className='text-left mx-auto flex md:flex-col gap-10 md:gap-6 rounded
+                     hover:shadow-blue-900 hover:shadow-xl transition-all duration-400 ease-in-out'>
                         
-                        <img src={ tool.image } alt={ tool.title } className='flex-1 h-50 max-w-1/2 md:max-w-full md:h-1/2' />
+                        <img src={ tool.image } alt={ tool.title } className='flex-1 max-w-1/2 md:max-w-full rounded' />
                         
-                        <div className='flex-1'>
+                        <div className='flex-1 pl-2 pr-1'>
                           <h2 className='text-lg font-semibold'>{ tool.title }</h2>
                           <p className='text-xs md:text-sm'>{ tool.desc }</p>
-                          <button className='text-sm md:text-md px-2 py-1 mt-4 border border-[#F51D28] rounded
-                          hover:bg-[#F51D28] hover:text-white'>
+                          <button className='text-sm md:text-md px-2 py-1 mt-4 text-gray-800 bg-gradient-to-r
+                          border border-blue-900 rounded hover:border-white hover:from-blue-900 hover:to-blue-500 hover:text-white'>
                               Lear More
                           </button>
                         </div>
