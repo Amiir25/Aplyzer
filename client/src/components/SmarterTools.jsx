11b7212aa6fca4e483bc5f4e3f5fc42de2bc356c
Amiir25 <amirsadik636@gmail.com>
2025-08-26 16:44:54 +0300

    Update the structure of the cards in SmarterTools.jsx

--- client/src/components/SmarterTools.jsx
+++ client/src/components/SmarterTools.jsx
@@ -13,18 +13,20 @@ const SmarterTools = () => {
         subtitle="Aplyzer combines AI and simplicity to help you track, optimize, and succeed in your job hunt." />
 
         {/* Cards */}
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
             {
                 tools.map((tool, i) => (
                     <div key={ i }
-                    className='text-left mx-auto flex md:flex-col gap-10 md:gap-6 rounded py-2
+                    className='text-left mx-auto flex md:flex-col rounded py-2 w-120
                      hover:shadow-blue-900 hover:shadow-xl transition-all duration-400 ease-in-out'>
                         
-                        <img src={ tool.image } alt={ tool.title } className='flex-1 max-w-1/2 md:max-w-full rounded' />
+                        <div className='px-6 mb-4'>
+                          <img src={ tool.image } alt={ tool.title } className='w-full rounded' />
+                        </div>
                         
-                        <div className='flex-1 pl-2 pr-1'>
-                          <h2 className='text-lg font-semibold'>{ tool.title }</h2>
-                          <p className='text-xs md:text-sm'>{ tool.desc }</p>
+                        <div className='flex-1 px-6'>
+                          <h2 className='text-3xl font-bold my-2'>{ tool.title }</h2>
+                          <p className='text-lg'>{ tool.desc }</p>
                           <button className='text-sm md:text-md px-2 py-1 mt-4 text-gray-800 bg-gradient-to-r
                           border border-blue-900 rounded hover:border-white hover:from-blue-900 hover:to-blue-500 hover:text-white'>
                               Lear More
