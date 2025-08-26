8a3081cd9f7efb0d7f6d078ae958e807c092c327
Amiir25 <amirsadik636@gmail.com>
2025-08-26 18:22:25 +0300

    Add align props to define the alignment of the titles in Title.jsx

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Title = ({ title, subtitle }) => {
+const Title = ({ title, subtitle, align }) => {
   return (
     <>
-    <div className='text-center mt-40 md:mb-10'>
+    <div className={`text-${align} mt-40 md:mb-10`}>
         <h1 className='text-3xl md:text-4xl text-gray-800 font-semibold'>{ title }</h1>
-        <p className='md:text-lg text-gray-800 font-light px-6 my-4 -mt-1'>{ subtitle }</p>
+        <p className='md:text-lg text-gray-800 font-light my-4 -mt-1'>{ subtitle }</p>
     </div>
     </>
   )
