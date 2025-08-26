0d12485a38ca8e7595736d087dff863df2a9de65
Amiir25 <amirsadik636@gmail.com>
2025-08-26 17:02:52 +0300

    Update the style of the button in LookInside.jsx

--- client/src/components/LookInside.jsx
+++ client/src/components/LookInside.jsx
@@ -14,8 +14,9 @@ const LookInside = () => {
       className={`h-screen bg-cover bg-center bg-no-repeat bg-fixed`}>
       </div>
 
-      <button className='border w-1/3 py-2 mt-10 md:text-2xl font-medium rounded border border-[#02A9EB]
-      hover:text-gray-200 hover:bg-gradient-main '>
+      <button className='w-1/3 py-2 mt-10 md:text-2xl font-medium rounded border border-blue-500
+      bg-gradient-to-r hover:from-blue-900 hover:to-blue-500 hover:text-white hover:border-white
+      hover:opacity-90 cursor-pointer active:opacity-100'>
         Try it now!
       </button>
     </div>
