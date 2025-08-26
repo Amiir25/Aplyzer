0371347d271b2925cb4779c7fb39a2eab66cbe48
Amiir25 <amirsadik636@gmail.com>
2025-08-26 18:21:05 +0300

    Update the design of CTA section in CTA.jsx

--- client/src/components/CTA.jsx
+++ client/src/components/CTA.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import Title from './Title'
 import { assets } from '../assets/assets'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 const CTA = () => {
   return (
     <>
     <div style={{ backgroundImage: `url(${assets.bgCTA})` }}
-    className='px-6 md:px-16 lg:px-24 xl:px-32 h-screen border border-white bg-cover bg-center bg-no-repeat'>
+    className='h-screen bg-cover bg-center bg-no-repeat'>
 
-        <Title
-        title="Smarter Job Hunting Starts Now"
-        subtitle="Aplyzer gives you the tools to organize, optimize, and succeed." />
+        <div className='px-6 md:px-16 lg:px-24 xl:px-32
+        flex flex-col items-center justify-center gap-2 h-full w-2/3'>
 
-        {/* CTA buttons */}
-        <div className='flex justify-center gap-12'>
-          
-          <button className='text-lg md:text-2xl font-medium px-4 py-2 md:px-6 md:py-4 rounded bg-gradient-main text-white hover:opacity-90 '>
-            Get Started Free
+          <h1 className='text-6xl text-gray-800 text-cente font-bold'>Smarter Job Hunting Starts Here</h1>
+          <p className='text-2xl text-center text-gray-600 my-4'>Take control of your applications, track your progress, and land your dream job with ease.</p>
+
+          {/* CTA button */}
+          <button className='relative text-lg md:text-4xl text-white font-medium py-2 md:py-6 w-full mt-10 mx-6 rounded
+          bg-gradient-to-r from-blue-900 to-blue-500 hover:opacity-90 cursor-pointer active:opacity-100 '>
+            Get Started Now
+
+            <FontAwesomeIcon icon={ faArrowRight }
+            className='absolute top-1/5 -right-4 w-10 h-10 rounded-full p-2 bg-blue-500' />
           </button>
 
-          <button className='text-lg md:text-2xl font-medium border px-4 py-2 md:px-6 md:py-4 rounded border-[#02A9EB] hover:text-gray-600 '>
-            See How It Works
-          </button>
         </div>
     </div>
     </>
