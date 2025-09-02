b29e9c56d62684ad268a5c85e4ae09528a067858
Amiir25 <amirsadik636@gmail.com>
2025-09-02 16:47:30 +0300

    Update class to className in Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -78,11 +78,11 @@ const Footer = () => {
         &nbsp;•  &copy; 2025 Aplyzer •
     </p> */}
 
-    <footer class="px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-300 bg-black">
-        <div class="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500/30 pb-6">
-            <div class="w-1/3">
+    <footer className="px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-300 bg-black">
+        <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500/30 pb-6">
+            <div className="w-1/3">
                 <img src='/logo.png' alt="Logo Image" className='w-full' />
-                <div class="flex items-start gap-6 mt-3">
+                <div className="flex items-start gap-6 mt-3">
                     <a href="#">
                         <FontAwesomeIcon icon={ faGithub } className='md:text-xl text-white hover:text-gray-400 active:text-white' />
                     </a>
@@ -97,10 +97,10 @@ const Footer = () => {
                     </a>
                 </div>
             </div>
-            <div class="flex items-start md:justify-evenly gap-20 w-1/2">
+            <div className="flex items-start md:justify-evenly gap-20 w-1/2">
                 <div>
-                    <h2 class="font-semibold mb-5 text-gray-500">Company</h2>
-                    <ul class="text-sm space-y-2">
+                    <h2 className="font-semibold mb-5 text-gray-500">Company</h2>
+                    <ul className="text-sm space-y-2">
                         <li><a href="#">Home</a></li>
                         <li><a href="#">About us</a></li>
                         <li><a href="#">Contact us</a></li>
@@ -108,15 +108,15 @@ const Footer = () => {
                     </ul>
                 </div>
                 <div>
-                    <h2 class="font-semibold mb-5 text-gray-500">Get in touch</h2>
-                    <div class="text-sm space-y-2">
+                    <h2 className="font-semibold mb-5 text-gray-500">Get in touch</h2>
+                    <div className="text-sm space-y-2">
                         <p>+251 944 081 246</p>
                         <p>amirsadik636@gmail.com</p>
                     </div>
                 </div>
             </div>
         </div>
-        <p class="pt-4 text-center text-xs md:text-sm pb-5">
+        <p className="pt-4 text-center text-xs md:text-sm pb-5">
             Copyright 2025 © <a href="#">Aplyzer</a>. All Right Reserved.
         </p>
     </footer>
