250a7ef351204653a55487942ec2e22ca373419b
Amiir25 <amirsadik636@gmail.com>
2025-09-02 17:42:01 +0300

    Make the Smarter Tools section responsive in SmarterTools.jsx

--- client/src/components/SmarterTools.jsx
+++ client/src/components/SmarterTools.jsx
@@ -13,11 +13,11 @@ const SmarterTools = () => {
         subtitle="Aplyzer combines AI and simplicity to help you track, optimize, and succeed in your job hunt." />
 
         {/* Cards */}
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
             {
                 tools.map((tool, i) => (
                     <div key={ i }
-                    className='text-left mx-auto flex md:flex-col rounded py-2 w-120
+                    className='text-left mx-auto rounded py-2 w-120 md:w-160 lg:w-120
                      hover:shadow-[0_0_50px_rgb(30,58,138,0.8)]  transition-all duration-400 ease-in-out'>
                         
                         <div className='px-6 mb-4'>
@@ -25,8 +25,8 @@ const SmarterTools = () => {
                         </div>
                         
                         <div className='flex-1 px-6'>
-                          <h2 className='text-3xl font-bold my-2'>{ tool.title }</h2>
-                          <p className='text-lg'>{ tool.desc }</p>
+                          <h2 className='text-2xl md:text-3xl font-bold my-2'>{ tool.title }</h2>
+                          <p className='md:text-lg'>{ tool.desc }</p>
                           <button className='text-sm md:text-md px-2 py-1 mt-4 text-gray-800 bg-gradient-to-r
                           border border-blue-900 rounded hover:border-white hover:from-blue-900 hover:to-blue-500 hover:text-white'>
                               Lear More
