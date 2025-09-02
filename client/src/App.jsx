d66a17e5c778edf3d974c5f9aac6faf0eec20fe6
Amiir25 <amirsadik636@gmail.com>
2025-09-02 14:40:27 +0300

    Add framer-motion for simple animation on page change in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,33 +10,62 @@ import AllJobs from './user/pages/AllJobs'
 import JobDetails from './user/pages/JobDetails'
 import AddNewJob from './user/pages/AddNewJob'
 import UpdateJob from './user/pages/UpdateJob'
+import { AnimatePresence, motion } from "framer-motion";
 
 const App = () => {
 
   const location = useLocation();
 
+  // return (
+  //   <div className=''>
+
+  //       <Routes>
+
+  //         Home routes
+  //         <Route path='/' element={ <Home/> } />
+  //         <Route path='/auth/signup' element={ <SignUp/> } />
+  //         <Route path='/auth/signin' element={ <SignIn/> } />
+
+  //         User routes
+  //         <Route path='/user/dashboard/:userId' element={ <Dashboard/> } />
+  //         <Route path='/user/all-jobs/:userId' element={ <AllJobs/> } />
+  //         <Route path='/user/job-details/:jobId' element={ <JobDetails/> } />
+  //         <Route path='/user/add-new-job/:userId' element={ <AddNewJob/> } />
+  //         <Route path='/user/update-job/:jobId' element={ <UpdateJob/> } />
+          
+  //       </Routes>
+      
+  //     {/* Footer */}
+  //     <Footer/>
+  //   </div>
+  // )
+
   return (
-    <div className=''>
-      
-      <Routes>
+    // Framer motion for animation on page change
+    <motion.div
+      key={location.pathname}
+      initial={{ scale: 0.98, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1 }}
+      exit={{ scale: 0.98, opacity: 0 }}
+      transition={{ duration: 0.4, ease: "easeInOut" }}
+      className="min-h-screen"
+    >
+         <Routes>
 
-        {/* Home routes */}
-        <Route path='/' element={ <Home/> } />
-        <Route path='/auth/signup' element={ <SignUp/> } />
-        <Route path='/auth/signin' element={ <SignIn/> } />
+           {/* Home routes */}
+           <Route path='/' element={ <Home/> } />
+           <Route path='/auth/signup' element={ <SignUp/> } />
+           <Route path='/auth/signin' element={ <SignIn/> } />
 
-        {/* User routes */}
-        <Route path='/user/dashboard/:userId' element={ <Dashboard/> } />
-        <Route path='/user/all-jobs/:userId' element={ <AllJobs/> } />
-        <Route path='/user/job-details/:jobId' element={ <JobDetails/> } />
-        <Route path='/user/add-new-job/:userId' element={ <AddNewJob/> } />
-        <Route path='/user/update-job/:jobId' element={ <UpdateJob/> } />
-        
-      </Routes>
-      
-      {/* Footer */}
-      <Footer/>
-    </div>
+           {/* User routes */}
+           <Route path='/user/dashboard/:userId' element={ <Dashboard/> } />
+           <Route path='/user/all-jobs/:userId' element={ <AllJobs/> } />
+           <Route path='/user/job-details/:jobId' element={ <JobDetails/> } />
+           <Route path='/user/add-new-job/:userId' element={ <AddNewJob/> } />
+           <Route path='/user/update-job/:jobId' element={ <UpdateJob/> } />
+          
+         </Routes>
+    </motion.div>
   )
 }
 
