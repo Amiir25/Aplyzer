43e545bbde9683f981c553691b2b18c10d284621
Amiir25 <amirsadik636@gmail.com>
2025-09-03 06:52:27 +0300

    Make the CTA section responsive in CTA.jsx

--- client/src/components/CTA.jsx
+++ client/src/components/CTA.jsx
@@ -8,21 +8,25 @@ const CTA = () => {
   return (
     <>
     <div style={{ backgroundImage: `url(${assets.bgFAQAndCTA})` }}
-    className='h-screen bg-cover bg-center bg-no-repeat mt-20'>
+    className='h-[70%] md:h-screen bg-cover bg-center bg-no-repeat mt-20 py-10'>
 
         <div className='px-6 md:px-16 lg:px-24 xl:px-32
-        flex flex-col items-center justify-center gap-2 h-full w-2/3'>
+        flex flex-col items-center md:items-left justify-center gap-2 h-full w-full sm:w-2/3'>
 
-          <h1 className='text-6xl text-gray-800 text-cente font-bold'>Smarter Job Hunting Starts Here</h1>
-          <p className='text-2xl text-center text-gray-600 my-4'>Take control of your applications, track your progress, and land your dream job with ease.</p>
+          <h1 className='text-4xl sm:text-5xl lg:text-6xl text-gray-800 text-center sm:text-left font-bold'>
+            Smarter Job Hunting Starts Here
+          </h1>
+          <p className='text-xl lg:text-2xl text-gray-600 text-center md:text-left lg:text-center my-4'>
+            Take control of your applications, track your progress, and land your dream job with ease.
+          </p>
 
           {/* CTA button */}
-          <button className='relative text-lg md:text-4xl text-white font-medium py-2 md:py-6 w-full mt-10 mx-6 rounded
+          <button className='relative text-2xl sm:text-3xl lg:text-4xl text-white font-medium py-4 md:py-6 w-60 sm:w-80 lg:w-full mt-10 rounded
           bg-gradient-to-r from-blue-900 to-blue-500 hover:opacity-90 cursor-pointer active:opacity-100 '>
             Get Started Now
 
             <FontAwesomeIcon icon={ faArrowRight }
-            className='absolute top-1/5 -right-4 w-10 h-10 rounded-full p-2 bg-blue-500
+            className='absolute top-1/5 -right-4 text-xl md:text-2xl lg:text-3xl xl:text-4xl rounded-full p-2 bg-blue-500
             shadow-xl shadow-black '/>
           </button>
 
