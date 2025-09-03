9cf0764274ebb2062dc4c8dec802f3a11d947ca9
Amiir25 <amirsadik636@gmail.com>
2025-09-03 10:54:34 +0300

    Use map method to display cards in StatusSummery.jsx

--- client/src/user/components/StatusSummery.jsx
+++ client/src/user/components/StatusSummery.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClockFour, faFileLines, faUserCheck, faXmarkCircle } from '@fortawesome/free-solid-svg-icons';
-import { assets } from '../../assets/assets';
+import { assets, statusSummery } from '../../assets/assets';
 
 const StatusSummery = ({ jobs }) => {
 
+    const total = jobs;
     const interviewed = jobs.filter(job => job.status === 'Interviewed');
     const rejected = jobs.filter(job => job.status === 'Rejected');
     const waiting = jobs.filter(job => job.status === 'Waiting' || job.status === 'Applied');
+
+    // Colors for Summeries
+    const summeryColor = {
+        Total: 'sky-500',
+        Interviewed: 'green-500',
+        Rejected: 'red-500',
+        Waiting: 'amber-500',
+    }
+
+    // Data for Summeries
+    const summeryData = {
+        Total: jobs,
+        Interviewed: interviewed,
+        Rejected: rejected,
+        Waiting: waiting,
+    }
 
     return (
         <>
@@ -15,59 +32,25 @@ const StatusSummery = ({ jobs }) => {
             <h1 className='text-2xl md:text-4xl font-semibold mb-8'>Stats Summery</h1>
             
             {/* Cards */}
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+            <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-x-4 gap-y-10'>
+                {
+                    statusSummery.map((summery, i) => (
+                        <div key={i}
+                        className={`min-h-68 w-fit py-2 shadow-xl border-t-6 border-${summeryColor[summery.title]}`}>
+                            <img src={ summery.image } alt={`${summery.title} Image`} className='w-full h-[100px] mx-auto m-2'/>
+                            <div className='px-4'>
+                                <h1 className='text-2xl font-bold'>{ summery.title }</h1>
+                                <p className='text-gray-700 text-sm'>{ summery.desc }</p>
+                            </div>
+                            <p className={`mt-8 text-xl font-black bg-${summeryColor[summery.title]}/20 w-fit 
+                            px-2 text-${summeryColor[summery.title]}`}>
+                                { summeryData[summery.title].length }
+                            </p>
+                        </div>
+                    ))
+                }
 
-                {/* Total */}
-                <div className='relative h-64 w-fit px-4 py-2 shadow-xl border-t-6 border-sky-500'>
-                    <img src={ assets.file } alt="3D File Icon" className='w-1/3 mx-auto m-2'/>
-                    <div>
-                        <h1 className='text-2xl font-bold'>Total</h1>
-                        <p className='text-gray-700 text-sm'>All applications you've submitted so far.</p>
-                    </div>
-                    <p className='absolute bottom-4 left-4 mt-2 text-xl font-black bg-sky-500/20 w-fit 
-                    px-2 text-sky-500'>
-                        { jobs.length }
-                    </p>
-                </div>
                 
-                {/* Interviwed */}
-                <div className='relative h-64 w-fit px-4 py-2 shadow-xl border-t-6 border-green-500'>
-                    <img src={ assets.user } alt="3D File Icon" className='w-1/3 mx-auto m-2'  />
-                    <div>
-                        <h1 className='text-2xl font-bold'>Interviewed</h1>
-                        <p className='text-gray-700 text-sm'>Applications that led to interviews.</p>
-                    </div>
-                    <p className='absolute bottom-4 left-4 mt-2 text-xl font-black bg-green-500/20 w-fit px-2
-                        text-green-500'>
-                        { interviewed.length }
-                    </p>
-                </div>
-
-                {/* Rejected */}
-                <div className='relative h-64 w-fit px-4 py-2 shadow-xl border-t-6 border-red-500'>
-                    <img src={ assets.x } alt="3D File Icon" className='w-1/3 mx-auto m-2'  />
-                    <div>
-                        <h1 className='text-2xl font-bold'>Rejected</h1>
-                        <p className='text-gray-700 text-sm'>Applications that were declined or marked unsuccessful.</p>
-                    </div>
-                    <p className='absolute bottom-4 left-4 mt-2 text-xl font-black bg-red-500/20 w-fit px-2
-                        text-red-500'>
-                        { rejected.length }
-                    </p>
-                </div>
-
-                {/* Waiting */}
-                <div className='relative h-64 w-fit px-4 py-2 shadow-xl border-t-6 border-amber-500'>
-                    <img src={ assets.clock } alt="3D File Icon" className='w-1/3 mx-auto m-2'  />
-                    <div>
-                        <h1 className='text-2xl font-bold'>Waiting</h1>
-                        <p className='text-gray-700 text-sm'>Applications with no response yet or still in progress.</p>
-                    </div>
-                    <p className='absolute bottom-4 left-4 mt-2 text-xl font-black bg-amber-500/20 w-fit px-2
-                        text-amber-500'>
-                        { waiting.length }
-                    </p>
-                </div>
 
             </div>
         </div>
