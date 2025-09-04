379f0b1743198041503a7d47e675968f0f6817d3
Amiir25 <amirsadik636@gmail.com>
2025-09-04 18:13:35 +0300

    Change the place of FavoriteJobs.jsx in QuickActions.jsx

--- client/src/user/components/QuickActions.jsx
+++ client/src/user/components/QuickActions.jsx
@@ -2,29 +2,23 @@ import React from 'react'
 
 const QuickActions = () => {
 
+    const buttons = ['Add New Job', 'Check Resume', 'Match Job Description', 'Generate Cover Letter'];
+
     return (
         <>
         <div className='px-6 md:px-12 lg:px-24 xl:px-32 my-40'>
 
-            <h1 className='text-2xl md:text-4xl font-semibold mb-8'>Quick Actions</h1>
+            <h1 className='text-2xl md:text-4xl font-semibold mb-4'>Quick Actions</h1>
 
-            <div className='flex flex-col md:flex-row gap-10'>
-                <button className='border w-64 py-2 mt-8 md:text-xl font-medium border border-[#02A9EB]
-                    hover:text-gray-200 hover:bg-gradient-main rounded-lg'>
-                    Add new job
-                </button>
-                <button className='border w-64 py-2 mt-8 md:text-xl font-medium border border-[#02A9EB]
-                    hover:text-gray-200 hover:bg-gradient-main rounded-lg'>
-                    Check resume
-                </button>
-                <button className='border w-64 py-2 mt-8 md:text-xl font-medium border border-[#02A9EB]
-                    hover:text-gray-200 hover:bg-gradient-main rounded-lg'>
-                    Macth job description
-                </button>
-                <button className='border w-64 py-2 mt-8 md:text-xl font-medium border border-[#02A9EB]
-                    hover:text-gray-200 hover:bg-gradient-main rounded-lg'>
-                    Generate cover letter
-                </button>
+            <div className='grid grid-cols-2 lg:grid-cols-4 gap-x-4'>
+                {
+                    buttons.map((button, i) => (
+                        <button className='py-2 mt-8 text-sm md:text-xl font-medium border border-blue-500 rounded
+                        hover:text-white hover:bg-gradient-to-r hover:from-blue-900 hover:to-blue-500'>
+                            { button }
+                        </button>
+                    ))
+                }
             </div>
         </div>
         </>
