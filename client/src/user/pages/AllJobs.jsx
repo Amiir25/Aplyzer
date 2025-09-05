2d9c09e6e426b522d0af22e2af5974849c9c699f
Amiir25 <amirsadik636@gmail.com>
2025-09-05 07:22:08 +0300

    Remove UserNavbar from AllJobs.jsx

--- client/src/user/pages/AllJobs.jsx
+++ client/src/user/pages/AllJobs.jsx
@@ -107,112 +107,110 @@ const AllJobs = () => {
 
     return (
         <>
-            <UserNavbar />
+        <div className='px-6 md:px-12 lg:px-24 xl:px-32'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 my-20'>
+                {
+                    allJobs.map((job, i) => (
+                        <div key={ i }
+                            className='relative m-2 pt-10 pb-2 px-4 shadow shadow-gray-900 rounded'>
 
-            <div className='px-6 md:px-12 lg:px-24 xl:px-32'>
-                <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 my-20'>
-                    {
-                        allJobs.map((job, i) => (
-                            <div key={ i }
-                                className='relative m-2 pt-10 pb-2 px-4 shadow shadow-gray-900 rounded'>
+                            {/* Status */}
+                            <p className={`absolute top-0 left-0 text-lg text-gray-100 font-medium px-2 py-1 bg-ora
+                            bg-gradient-to-r ${ statusColor[job.status] }`
+                            }>
+                                {job.status}
+                            </p>
 
-                                {/* Status */}
-                                <p className={`absolute top-0 left-0 text-lg text-gray-100 font-medium px-2 py-1 bg-ora
-                                bg-gradient-to-r ${ statusColor[job.status] }`
-                                }>
-                                    {job.status}
-                                </p>
+                            {/* Star */}
+                            <button
+                                onClick={() => toggleFavorite(job.jid)}
+                                className={`absolute top-1 right-1 text-lg text-gray-800 font-medium`}>
+                                <FontAwesomeIcon icon={favorite[job.jid] ? solidStar : regularStar}
+                                    className={`${favorite[job.jid] && 'text-orange-500'}`} />
+                            </button>
 
-                                {/* Star */}
-                                <button
-                                    onClick={() => toggleFavorite(job.jid)}
-                                    className={`absolute top-1 right-1 text-lg text-gray-800 font-medium`}>
-                                    <FontAwesomeIcon icon={favorite[job.jid] ? solidStar : regularStar}
-                                        className={`${favorite[job.jid] && 'text-orange-500'}`} />
-                                </button>
+                            {/* Title & Experiance level */}
+                            <div>
+                                <span className='text-2xl md:text-3xl font-bold'>{job.job_title}</span>
+                                <small className='text-xs text-pink-500 ml-2'>{ job.exp_level }</small>
+                            </div>
 
-                                {/* Title & Experiance level */}
-                                <div>
-                                    <span className='text-2xl md:text-3xl font-bold'>{job.job_title}</span>
-                                    <small className='text-xs text-pink-500 ml-2'>{ job.exp_level }</small>
-                                </div>
-
-                                {/* Company, Location, Type, and Mode */}
-                                <div className='flex items-center justify-between'>
-                                    <div className='text-gray-700'>
-                                        <p className='text-lg md:text-xl'>{job.company_name}</p>
-                                        <p className='text-sm'>
-                                            <FontAwesomeIcon icon={faLocationDot} className='me-1' />
-                                            {job.location}
-                                        </p>
-                                    </div>
-                                    <p className='min-w-fit text-xs bg-gray-200 px-2 py-1 text-fuchsia-800 rounded-full'>
-                                        <FontAwesomeIcon icon={faCircleDot} className='me-1' />
-                                        {job.job_type} / {job.work_mode}
+                            {/* Company, Location, Type, and Mode */}
+                            <div className='flex items-center justify-between'>
+                                <div className='text-gray-700'>
+                                    <p className='text-lg md:text-xl'>{job.company_name}</p>
+                                    <p className='text-sm'>
+                                        <FontAwesomeIcon icon={faLocationDot} className='me-1' />
+                                        {job.location}
                                     </p>
                                 </div>
+                                <p className='min-w-fit text-xs bg-gray-200 px-2 py-1 text-fuchsia-800 rounded-full'>
+                                    <FontAwesomeIcon icon={faCircleDot} className='me-1' />
+                                    {job.job_type} / {job.work_mode}
+                                </p>
+                            </div>
 
-                                {/* Dates */}
-                                <div className='grid grid-cols-2 gap-10 text-sm my-6 border-t border-b border-gray-300
-                                py-3'>
-                                    <div>
-                                        <p>Applied Date</p>
-                                        <p className='text-xs'>{ dayjs(job.applied_date).format('DD-MM-YYYY') }</p>
-                                    </div>
-                                    <div>
-                                        <p>Deadline</p>
-                                        <p className='text-xs text-red-700'>{ dayjs(job.deadline).format('DD-MM-YYYY') }</p>
-                                    </div>
+                            {/* Dates */}
+                            <div className='grid grid-cols-2 gap-10 text-sm my-6 border-t border-b border-gray-300
+                            py-3'>
+                                <div>
+                                    <p>Applied Date</p>
+                                    <p className='text-xs'>{ dayjs(job.applied_date).format('DD-MM-YYYY') }</p>
                                 </div>
-
-                                {/* Buttons */}
-                                <div className='flex items-center justify-around gap-4 mt-8'>
-                                    
-                                    {/* View button */}
-                                    <button
-                                    onClick={() => openJobDetail(job.jid)}
-                                    className='px-3 py-1 w-1/3 rounded border border-blue-500 hover:text-white hover:bg-blue-500'>
-                                        View
-                                    </button>
-
-                                    {/* Update button */}
-                                    <button
-                                    onClick={() => updateJob(job.jid)}
-                                    className='px-3 py-1 w-1/3 rounded border border-green-500 hover:text-white hover:bg-green-500'>
-                                        Update
-                                    </button>
-
-                                    {/* Delete button */}
-                                    <button
-                                    onClick={ () => onDelete(job) }
-                                    className='px-3 py-1 w-1/3 rounded border border-red-500 hover:text-white hover:bg-red-500'>
-                                        Delete
-                                    </button>
+                                <div>
+                                    <p>Deadline</p>
+                                    <p className='text-xs text-red-700'>{ dayjs(job.deadline).format('DD-MM-YYYY') }</p>
                                 </div>
                             </div>
-                        ))
-                    }
 
-                    {/* Delete Job */}
-                    {
-                        selectedJob &&
-                        <DeleteJob
-                            jobId = { selectedJob.jid }
-                            job_title={ selectedJob.job_title }
-                            company_name={ selectedJob.company_name }
-                            onClose = { handlePopupClose }
-                            onDelete = { handleDelete } />
-                    }
-                </div>
+                            {/* Buttons */}
+                            <div className='flex items-center justify-around gap-4 mt-8'>
+                                
+                                {/* View button */}
+                                <button
+                                onClick={() => openJobDetail(job.jid)}
+                                className='px-3 py-1 w-1/3 rounded border border-blue-500 hover:text-white hover:bg-blue-500'>
+                                    View
+                                </button>
 
-                {/* Add new job Button */}
-                <button className='my-10 text-xl text-white px-8 py-4 rounded-lg bg-gradient-to-r from-blue-900 to-blue-500 hover
-                hover:opacity-90 cursor-pointer active:opacity-100'>
-                    <Link to={`/user/add-new-job/${userId}`}>
-                        Add new job
-                    </Link>
-                </button>
+                                {/* Update button */}
+                                <button
+                                onClick={() => updateJob(job.jid)}
+                                className='px-3 py-1 w-1/3 rounded border border-green-500 hover:text-white hover:bg-green-500'>
+                                    Update
+                                </button>
+
+                                {/* Delete button */}
+                                <button
+                                onClick={ () => onDelete(job) }
+                                className='px-3 py-1 w-1/3 rounded border border-red-500 hover:text-white hover:bg-red-500'>
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
+                    ))
+                }
+
+                {/* Delete Job */}
+                {
+                    selectedJob &&
+                    <DeleteJob
+                        jobId = { selectedJob.jid }
+                        job_title={ selectedJob.job_title }
+                        company_name={ selectedJob.company_name }
+                        onClose = { handlePopupClose }
+                        onDelete = { handleDelete } />
+                }
             </div>
+
+            {/* Add new job Button */}
+            <button className='my-10 text-xl text-white px-8 py-4 rounded-lg bg-gradient-to-r from-blue-900 to-blue-500 hover
+            hover:opacity-90 cursor-pointer active:opacity-100'>
+                <Link to={`/user/add-new-job/${userId}`}>
+                    Add new job
+                </Link>
+            </button>
+        </div>
         </>
     )
 }
