fd7b28277dcdb39f6ea0cdb24defa8979faec0a7
Amiir25 <amirsadik636@gmail.com>
2025-09-09 21:47:12 +0300

    Add authentication middleware in authMiddleware.js

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+const authenticateToken = (req, res, next) => {
+     // Get the token from Authorization header
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // header format: "Bearer <token>"
+    
+    if (!token) return res.status(401).json({ message: 'No token provided' });
+
+    // Verify token
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.status().json({ message: 'Invalid or expired token' });
+
+        // Save payload to request object
+        req.user = user;
+        next();
+    })
+}
+
+export default authenticateToken;
