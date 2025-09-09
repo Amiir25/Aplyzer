ce0576928079ba58f7f35e016c91f82e52873771
Amiir25 <amirsadik636@gmail.com>
2025-09-09 21:47:41 +0300

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -4,7 +4,6 @@ const authenticateToken = (req, res, next) => {
      // Get the token from Authorization header
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1]; // header format: "Bearer <token>"
-    
     if (!token) return res.status(401).json({ message: 'No token provided' });
 
     // Verify token
