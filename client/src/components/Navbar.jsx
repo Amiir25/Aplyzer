571aff18d11ed275b2bac70fedc1c9e46d344f2e
Amiir25 <amirsadik636@gmail.com>
2025-09-12 10:07:12 +0300

    Hide navbar links from job details page in Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
                 </Link>
             }
 
-            {/* Sign In button */}
+            {/* --- */}
             {
                 !user ?
 
@@ -56,66 +56,31 @@ const Navbar = () => {
                 
                 <>
                 {/* Links */}
-                <div className="relative gap-8 hidden md:flex">
-                    <Link to='/' className="text-sm hover:text-gray-600">Resume Builder</Link>
-                    <Link to='/' className="text-sm hover:text-gray-600">Resume Job Matching</Link>
-                    <Link to='/' className="text-sm hover:text-gray-600">Cover Letter Generator</Link>
+                {
+                    // Hide navbar links from job details page
+                    !location.pathname.includes('job-details') &&
+                    <div className="relative gap-8 hidden md:flex">
+                        <Link to={`/user/dashboard/${user.id}`}
+                            className={`${ location.pathname.includes('/dashboard') && 'border-b-3 border-[#02A9EB]' }
+                            text-sm hover:text-gray-600`}>
+                            Dashboard
+                        </Link>
+            
+                        <Link to={`/user/all-jobs/${user.id}`}
+                            className={`${ location.pathname.includes('/all-jobs') && 'border-b-3 border-[#02A9EB]' }
+                            text-sm hover:text-gray-600`}>
+                            Jobs
+                        </Link>
+                        <Link to='/' className="text-sm hover:text-gray-600">Resume Builder</Link>
+                        <Link to='/' className="text-sm hover:text-gray-600">Resume Job Matching</Link>
+                        <Link to='/' className="text-sm hover:text-gray-600">Cover Letter Generator</Link>
+                    </div>
+                }
 
-                    {/* More tools */}
-                    {/* <div className="group">
-                        <button className=" flex items-center gap-2 hover:text-[#F51D28]">
-                            More
-                            <FontAwesomeIcon icon={ faAngleDown } />
-                        </button>
-
-                        <div className="absolute top-full left-0 right-0 px-2 pt-6 opacity-0 max-h-0 z-10
-                        transform translate-y-full overflow-hidden transition-all ease-in-out duration-200
-                        group-hover:max-h-82 group-hover:translate-y-0 group-hover:border-b border-gray-400
-                        group-hover:opacity-100">
-                            
-                            <Link to='/' className="flex items-center gap-4 mb-6 hover:text-gray-600">
-                                <FontAwesomeIcon icon={ faChartBar } className="w-8 p-2 bg-rose-400/50 text-rose-700
-                                rounded text-2xl" />
-                                <div>
-                                    <h2 className="text-sm font-bold">Job Application Tracker</h2>
-                                    <p className="text-xs">Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                            </Link>
-
-                            <Link to='/' className="flex items-center gap-4 mb-6 hover:text-gray-600">
-                                <FontAwesomeIcon icon={ faCheckDouble } className="w-8 p-2 bg-yellow-400/50 text-yellow-700
-                                rounded text-2xl" />
-                                <div>
-                                    <h2 className="text-sm font-bold">AI Resume Cheker</h2>
-                                    <p className="text-xs">Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                            </Link>
-
-                            <Link to='/' className="flex items-center gap-4 mb-6 hover:text-gray-600">
-                                <FontAwesomeIcon icon={ faEquals } className="w-8 p-2 bg-lime-400/50 text-lime-700
-                                rounded text-2xl" />
-                                <div>
-                                    <h2 className="text-sm font-bold">Job Description Matching</h2>
-                                    <p className="text-xs">Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                            </Link>
-
-                            <Link to='/' className="flex items-center gap-4 mb-6 hover:text-gray-600">
-                                <FontAwesomeIcon icon={ faPen } className="w-8 p-2 bg-sky-400/50 text-sky-700
-                                rounded text-2xl" />
-                                <div>
-                                    <h2 className="text-sm font-bold">Cover Letter Generator</h2>
-                                    <p className="text-xs">Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                            </Link>
-                        </div>
-                    </div> more tools */}
-                    
-                </div> {/* links */}
                 
                 {/* Profile */}
-                <p className='flex items-center justify-center text-2xl md:text-4xl text-white w-12 h-12
-                    rounded-full bg-[#02A9EB]'>
+                <p className='flex items-center justify-center text-xl md:text-3xl text-white w-12 h-12
+                    rounded-full bg-gradient-to-r from-blue-900 to-blue-500'>
                     { user.username[0] }
                 </p>
                 </>
@@ -200,3 +165,57 @@ const Navbar = () => {
 }
 
 export default Navbar;
+
+
+/**
+More tools
+<div className="group">
+    <button className=" flex items-center gap-2 hover:text-[#F51D28]">
+        More
+        <FontAwesomeIcon icon={ faAngleDown } />
+    </button>
+
+    <div className="absolute top-full left-0 right-0 px-2 pt-6 opacity-0 max-h-0 z-10
+    transform translate-y-full overflow-hidden transition-all ease-in-out duration-200
+    group-hover:max-h-82 group-hover:translate-y-0 group-hover:border-b border-gray-400
+    group-hover:opacity-100">
+        
+        <Link to='/' className="flex items-center gap-4 mb-6 hover:text-gray-600">
+            <FontAwesomeIcon icon={ faChartBar } className="w-8 p-2 bg-rose-400/50 text-rose-700
+            rounded text-2xl" />
+            <div>
+                <h2 className="text-sm font-bold">Job Application Tracker</h2>
+                <p className="text-xs">Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+        </Link>
+
+        <Link to='/' className="flex items-center gap-4 mb-6 hover:text-gray-600">
+            <FontAwesomeIcon icon={ faCheckDouble } className="w-8 p-2 bg-yellow-400/50 text-yellow-700
+            rounded text-2xl" />
+            <div>
+                <h2 className="text-sm font-bold">AI Resume Cheker</h2>
+                <p className="text-xs">Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+        </Link>
+
+        <Link to='/' className="flex items-center gap-4 mb-6 hover:text-gray-600">
+            <FontAwesomeIcon icon={ faEquals } className="w-8 p-2 bg-lime-400/50 text-lime-700
+            rounded text-2xl" />
+            <div>
+                <h2 className="text-sm font-bold">Job Description Matching</h2>
+                <p className="text-xs">Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+        </Link>
+
+        <Link to='/' className="flex items-center gap-4 mb-6 hover:text-gray-600">
+            <FontAwesomeIcon icon={ faPen } className="w-8 p-2 bg-sky-400/50 text-sky-700
+            rounded text-2xl" />
+            <div>
+                <h2 className="text-sm font-bold">Cover Letter Generator</h2>
+                <p className="text-xs">Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+        </Link>
+    </div>
+</div> more tools
+
+ */
