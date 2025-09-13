d63844cabbdd938d11deb770dfc505fcda93666c
Amiir25 <amirsadik636@gmail.com>
2025-09-13 16:41:43 +0300

    Rename JWT_SECRET to ACCESS_TOKEN_SECRET in authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -7,8 +7,8 @@ const authenticateToken = (req, res, next) => {
     if (!token) return res.status(401).json({ message: 'No token provided' });
 
     // Verify token
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.status().json({ message: 'Invalid or expired token' });
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid or expired token' });
 
         // Save payload to request object
         req.user = user;
