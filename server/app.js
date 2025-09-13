a1980a07edbd65ea3e86f68fe0d48e789fec07e1
Amiir25 <amirsadik636@gmail.com>
2025-09-13 10:57:01 +0300

    Add cookieParser in app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import authRouter from './routes/authRouter.js';
 import userRouter from './routes/userRouter.js';
@@ -9,6 +10,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 // Routes
 app.use('/auth', authRouter);
