b296443c330f00931539d5fd895877f1db8532ec
Amiir25 <amirsadik636@gmail.com>
2025-09-13 10:53:59 +0300

    Create a new refresh end point for refreshToken controller in  authRouter.js

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { signIn, signUp } from "../controllers/authController.js";
+import { refreshToken, signIn, signUp } from "../controllers/authController.js";
 
 const authRouter = Router();
 
 authRouter.post('/sign-up', signUp);
 authRouter.post('/sign-in', signIn);
+authRouter.post('/refresh', refreshToken);
 
 export default authRouter;
