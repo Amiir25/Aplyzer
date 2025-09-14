ba772c33f661cd4c86ea5d01337668c77784e879
Amiir25 <amirsadik636@gmail.com>
2025-09-14 16:31:46 +0300

    Remove cookeiParser from app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
 
 import authRouter from './routes/authRouter.js';
 import userRouter from './routes/userRouter.js';
@@ -9,8 +8,7 @@ import userRouter from './routes/userRouter.js';
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
-app.use(cookieParser());
+app.use(cors({}));
 
 // Routes
 app.use('/auth', authRouter);
