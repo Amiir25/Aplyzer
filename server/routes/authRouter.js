1e1dc5cc0a5300117cb5a600a4220c9b9bb78b98
Amiir25 <amirsadik636@gmail.com>
2025-09-14 16:33:26 +0300

    Remove refresh end point from authRouter.js

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { refreshToken, signIn, signUp } from "../controllers/authController.js";
+import { signIn, signUp } from "../controllers/authController.js";
 
 const authRouter = Router();
 
 authRouter.post('/sign-up', signUp);
 authRouter.post('/sign-in', signIn);
-authRouter.post('/refresh', refreshToken);
 
 export default authRouter;
