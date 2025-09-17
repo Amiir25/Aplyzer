e5cb8ef60821f19061f4363c169b2e41d93fb901
Amiir25 <amirsadik636@gmail.com>
2025-09-17 17:43:18 +0300

    Update the text size of the titles in Title.jsx

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -4,8 +4,8 @@ const Title = ({ title, subtitle }) => {
   return (
     <>
     <div className={`text-center mt-40 md:mb-10`}>
-        <h1 className='text-3xl md:text-4xl text-gray-800 font-semibold'>{ title }</h1>
-        <p className='md:text-lg text-gray-800 font-light my-4 -mt-1'>{ subtitle }</p>
+        <h1 className='text-2xl md:text-3xl lg:text-4xl text-gray-800 font-semibold'>{ title }</h1>
+        <p className='text-sm md:text-lg text-gray-800 font-light my-4 px-2 -mt-1'>{ subtitle }</p>
     </div>
     </>
   )
