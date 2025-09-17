3744068017f599d5ec5e1cf3f316e5f6a77adfc7
Amiir25 <amirsadik636@gmail.com>
2025-09-17 17:54:38 +0300

    Update the size of the cta button in LookInside.jsx

--- client/src/components/LookInside.jsx
+++ client/src/components/LookInside.jsx
@@ -14,7 +14,7 @@ const LookInside = () => {
       className={`h-screen bg-cover bg-center bg-no-repeat bg-fixed`}>
       </div>
 
-      <button className='w-1/3 py-2 mt-10 md:text-2xl font-medium rounded border border-blue-500
+      <button className='w-60 md:w-80 lg:w-100 py-2 mt-10 md:text-2xl font-medium rounded border border-blue-500
       bg-gradient-to-r hover:from-blue-900 hover:to-blue-500 hover:text-white hover:border-white
       hover:opacity-90 cursor-pointer active:opacity-100'>
         Try it now!
